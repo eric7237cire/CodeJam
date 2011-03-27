629d4ee51b987c15bff1c4faa644a690aa9c0a1a
Eric G <eric7237cire@hotmail.com>
2011-03-27 16:06:48 +0200

    grid

--- 2008/apac_semi/apocalypse_soon.cxx
+++ 2008/apac_semi/apocalypse_soon.cxx
@@ -67,6 +67,71 @@ ostream& operator<<(ostream& os, const HW& hw) {
   return os;
 }
 
+bool do_turn(int meIndex, Grid<int>& grid) {
+  Grid<int> diffs(grid.rows, grid.cols);
+  diffs.reset(0);
+  LOG_OFF();
+  bool r = false;
+  
+  for(Grid<int>::iterator it = grid.begin(); it != grid.end(); ++it) 
+  {
+    LOG_STR(grid.getIndex(it) << " is value: " << *it);
+    
+    if (grid.getIndex(it) == meIndex) {
+      //LOG(meIndex);
+      continue;
+    }
+    if(*it == 0) {
+      //LOG_STR("It 0 skipping");
+      continue;
+    }
+    
+    vector<int> adjSquares = grid.getAdjacentSquaresIndexes(grid.getIndex(it));
+    
+    //Grid<int>::iterator strongestNeighbor = grid.end();
+    int strongestNeighbor = -1;
+    
+    for(vector<int>::const_iterator adj_it = adjSquares.begin();
+      adj_it != adjSquares.end();
+      ++adj_it) 
+    {
+      
+      //cout << "A" << endl;
+      if (grid[*adj_it] == 0) { //**adj_it == 0) {
+        continue;
+      }
+      
+      if (strongestNeighbor == -1 ||
+          grid[*adj_it] > grid[strongestNeighbor] ) 
+      {
+        r = true;
+        //cout << **adj_it << endl;
+        strongestNeighbor = *adj_it;    
+      }
+      //LOG(*strongestNeighbor);
+    }
+    
+    if (strongestNeighbor >= 0) {
+      diffs[strongestNeighbor] -= *it;
+      //LOG_STR("Diffs " << snIndex << " " << diffs[snIndex]);
+    }
+    
+  }
+  
+  //cout << "Diffs\n" << diffs;
+  
+  Grid<int>::iterator it = grid.begin();
+  Grid<int>::iterator diffs_it = diffs.begin();
+  for( ;it != grid.end(); ++it, ++diffs_it) 
+  {
+    int idx = grid.getIndex(it);
+    grid[idx] = max(0, grid[idx] + diffs[idx]);
+    //(*it) = max(*it + *diffs_it, 0);
+  }
+  
+  return r;
+}
+
 void do_test_case(int test_case, ifstream& input)
 {
   LOG_OFF();
@@ -86,7 +151,13 @@ void do_test_case(int test_case, ifstream& input)
     
   cout << "Case #" << (test_case+1) << ":" << endl;
     
+  cout << g << endl;
   
+  int t = 0;
+  while(do_turn(g.getIndex(r-1, c-1), g) && t < 14) {
+    ++t;
+    cout << g << endl;
+  }
   
   return;    
 }
--- 2008/apac_semi/grid.h
+++ 2008/apac_semi/grid.h
@@ -9,25 +9,198 @@ template<typename T> class Grid
 public:
   const unsigned int rows, cols;
   
-  typedef vector<T> VectorCells;
-  typedef vector<VectorCells> VectorRows;
-  VectorRows cells;
+  vector<T> cells;
   
-  Grid(unsigned int rows, unsigned int cols) : rows(rows), cols(cols), cells(rows)
+  Grid(unsigned int rows, unsigned int cols) : rows(rows), cols(cols), cells(rows * cols, T())
   {
-    for(typename VectorRows::iterator it = cells.begin(); it != cells.end(); ++it) {
-      *it = VectorCells(cols, T());      
-    }
+    
   }
   
-  int getIndex(int row, int col)
+  int getIndex(int row, int col) const
   {
     return row * cols + col;
   }
   
   void set(int row, int col, const T& val) {
-    cells[row][col] = val;
+    cells[getIndex(row, col)] = val;
   }
+  
+  void set(int index, const T& val) {
+    cells[index] = val;
+  }
+  
+  const T& operator[](int index) const {
+    assert(index >= 0 && index < rows * cols);
+    return cells[index];
+  }
+  T& operator[](int index) {
+    assert(index >= 0 && index < rows * cols);
+    return cells[index];
+  }
+  
+  void reset(const T& val) {
+    fill(cells.begin(), cells.end(), val);
+    
+  }
+  
+  class iterator;
+  
+  bool isValid(const iterator& it) {
+     int index = it.ptr - begin().ptr;
+     if (index >= 0 && index < rows*cols) {
+       return true;
+     }
+     return false;
+  }
+  
+  
+  
+  int getIndex(iterator it) {
+    return distance(begin().ptr, it.ptr); 
+  }
+  
+  vector<int> getAdjacentSquaresIndexes(int sqIndex) {
+    assert(sqIndex >= 0 && sqIndex < rows * cols);
+    const int row = sqIndex / cols;
+    const int col = sqIndex % cols;
+    vector<int> adjSquares;
+    
+    if (row > 0) {
+      adjSquares.push_back(sqIndex - cols);
+      assert(adjSquares.back() >= 0 && adjSquares.back() < rows * cols);
+    }
+    if (col > 0) {
+      adjSquares.push_back(sqIndex - 1);
+      assert(adjSquares.back() >= 0 && adjSquares.back() < rows * cols);
+    }
+    if (col < cols - 1) {
+      adjSquares.push_back(sqIndex + 1);
+      assert(adjSquares.back() >= 0 && adjSquares.back() < rows * cols);
+    }
+    if (row < rows - 1) {
+      adjSquares.push_back(sqIndex + cols);
+      assert(adjSquares.back() >= 0 && adjSquares.back() < rows * cols);
+    }
+    return adjSquares;
+  }
+  
+  
+  vector<iterator> getAdjacentSquares(iterator sq) {
+    const int index = getIndex(sq);
+    assert(index >= 0 && index < rows * cols);
+    const int row = index / cols;
+    const int col = index % cols;
+    assert(isValid(sq));
+    vector<iterator> adjSquares;
+    
+    if (row > 0) {
+      adjSquares.push_back(sq.up());
+      assert(isValid(adjSquares.back()));
+    }
+    if (col > 0) {
+      adjSquares.push_back(sq.left());
+      assert(isValid(adjSquares.back()));
+    }
+    if (col < cols - 1) {
+      adjSquares.push_back(sq.right());
+      assert(isValid(adjSquares.back()));
+    }
+    if (row < rows - 1) {
+      LOG(row);
+      adjSquares.push_back(sq.down());
+      assert(isValid(adjSquares.back()));
+    }
+    return adjSquares;
+  }
+  
+  iterator begin() {
+    return iterator(cells.begin(), cols);
+  }
+  
+  iterator end() {
+    return iterator(cells.end(), cols);
+  }
+  
+  class iterator {
+  public:
+    iterator(const iterator &other) :
+    ptr(other.ptr), cols(other.cols)
+    {
+    }
+    
+    iterator left() const {
+      return iterator(ptr - 1, cols);
+    }
+    
+    iterator right() const {
+      return iterator(ptr + 1, cols);
+    }
+    
+    iterator up() const {
+      return iterator(ptr - cols, cols);
+    }
+    
+    iterator down() const {
+      LOG(&*ptr);
+      LOG(cols);
+      return iterator(ptr + cols, cols);
+    }
+    
+    iterator &operator++() {
+      ++ptr;
+      return *this;
+    }
+    
+    iterator &operator--() {
+      --ptr;
+      return *this;
+    }
+    
+    iterator operator++(int) {
+      ++*this;
+      return iterator(ptr + 1);
+    }
+    
+    iterator operator--(int) {
+      --*this;
+      return iterator(ptr - 1);
+    }
+    
+    bool operator!=(const iterator& rhs) {
+      return ptr != rhs.ptr;
+    }
+    bool operator==(const iterator& rhs) {
+      return ptr == rhs.ptr;
+    }
+    
+    iterator& operator=(const iterator& rhs) {
+      if (&rhs != this) {
+        ptr = rhs.ptr;
+        assert(cols == rhs.cols);
+      }
+      return *this;
+    }
+    
+    T operator*() const {
+      return *ptr;
+    }
+    
+    T& operator*() {
+      return *ptr;
+    }
+    
+  private:
+    //iterator();
+    iterator(typename vector<T>::iterator ptr_, int cols) :
+    ptr(ptr_), cols(cols)
+    {
+    }
+    
+    typename vector<T>::iterator ptr;
+    const int cols;
+    
+    friend class Grid;
+  };
   
 };
 
@@ -36,17 +209,22 @@ template<typename T> ostream& operator<<(ostream& os, const Grid<T>& grid)
 {
   os << endl;
   for(unsigned int c = 0; c < grid.cols; ++c) {
-    os << (c % 10);
+    os << setw(5) << (c+1);
   }
-  os << endl << endl;
-  for(int r = grid.rows - 1; r >= 0; --r) {
+  os << endl;
+  string s(5, '_');
+  for(unsigned int c = 0; c < grid.cols; ++c) {
+    os << s;
+  }
+  os << endl;
+  for(int r = 0; r < grid.rows; ++r) {
     
     for(unsigned int c = 0; c < grid.cols; ++c) {
       
-      os << setw(5) << grid.cells[r][c];
+      os << setw(5) << grid.cells[grid.getIndex(r, c)];
       
     }
-    os << " :" << r;
+    os << "| " << (r+1);
     
     os << endl;
   }
