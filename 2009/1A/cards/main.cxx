88d280582efa48cfe250a2e7d03f1520d0fcc5f1
eric <eric@vickycat.(none)>
2012-03-01 08:55:15 +0100

    large

--- 2009/1A/cards/main.cxx
+++ 2009/1A/cards/main.cxx
@@ -24,6 +24,10 @@ using namespace std;
 
 void do_test_case(int, ifstream& input);
 
+typedef vector<double> VecDoub;
+
+VecDoub calcAverageResults;
+
 int main(int argc, char** args)
 {
     
@@ -42,7 +46,11 @@ int main(int argc, char** args)
 
   string dummy;
   getline(input, dummy);
-  SHOW_TIME_BEGIN(g) 
+  SHOW_TIME_BEGIN(g)
+
+
+calcAverageResults.resize(41*40*40, -1.0);
+
   	
   for (int test_case = 0; test_case < T; ++test_case) {
       
@@ -59,12 +67,70 @@ int fact(int x) {
     for(int i = 1; i <= x; ++i) {
         f *= i ;  
     }
+	LOG_STR("X " << x << " F " << f);
     return f;
 }
 
-int nCk (int n, int k) {
-  return fact(n) / ( fact(k) * fact(n-k) );  
+int fact(const int start, const int finish) {
+    int f = 1;
+    for(int i = start; i <= finish; ++i) {
+        f *= i ;  
+    }
+	LOG_STR("start " << start << " Finish " << finish);
+    return f;
+}
+
+long long nCk (int n, int k) {
+LOG_STR("n " << n << " k " << k);
+long long r = 0;
+
+if (k > (n-k) ) {
+   r = fact(k+1, n) / fact(n-k);
+} else {
+   r = fact(n-k+1, n) / fact(k);
+}
+   LOG(r);
+   return r;
 }  
+
+void nCkBigNumbers(int n, int k, vector<int>& numerator, vector<int>& denom) {
+ //n! / k! (n-k) !
+
+for (int i = 1; i <= n; ++i) {
+numerator.push_back(i);
+}
+
+for (int i = 1; i <= k; ++i) {
+denom.push_back(i);
+}
+
+for (int i = 1; i <= (n-k); ++i) {
+denom.push_back(i);
+}
+
+}
+
+double calc(  const vector<int>& numerator, const vector<int>& denom) {
+	unsigned int ni = 0;
+	unsigned int di = 0;
+
+	double r = 1;
+
+	const int LIMIT = 20000;
+
+	while(ni < numerator.size()) {
+		r *= numerator[ni++];
+		if (r >= LIMIT && di < denom.size())  {
+			r /= denom[di];
+		}
+	}
+
+	while(di < denom.size()) {
+		r /= denom[di++];
+	}
+
+	return r;
+}
 
 int countOnes(int x) {
     bitset<10> bs(x);
@@ -95,10 +161,18 @@ void gen_combinations(VecInt& list, const int N, const int C) {
     assert(expectedCount == list.size());
 }
 
+
+
 double calculateAverage(const int C, const int N, int taken) {
 //LOG_ON();
 
     LOG_STR("Calc average C= " << C << " N= " << N << " Taken " << taken);
+
+const int index = C * (40 * 40) + N * 40 + taken;
+if (calcAverageResults[index] > 0) {
+	return calcAverageResults[index];
+}
+
 
 if (taken == 0) {
 LOG_STR("First step, all cards count");
@@ -119,82 +193,64 @@ LOG_STR("First step, all cards count");
 	}
 
 
-    
-    int denom = nCk(C, N);
+    LOG_STR("Total combinations C= " << C << " N= " << N << " Taken " << taken);
+//    int denom = nCk(C, N);
+	vector<int> totalPossibleNum;
+	vector<int> totalPossibleDenom;
+	nCkBigNumbers(C, N, totalPossibleNum, totalPossibleDenom);
 
     //i is the # of cards that are in the deck that are currently not taken
     for(int i = 1; i <= remaining && i <= N; ++i) {
+	vector<int> coefNum;
+	vector<int> coefDenom;
+        nCkBigNumbers(remaining, i, coefNum, coefDenom);
+	nCkBigNumbers(taken, N-i, coefNum, coefDenom); 
 
-        int coef = nCk(remaining, i) * nCk(taken, N-i); 
-        LOG_STR("Coeff i=" << i << " coef = " << coef);
-	avg += (coef * (calculateAverage(C, N, taken+i) + 1)  / denom );
+	coefNum.insert(coefNum.end(), totalPossibleDenom.begin(), totalPossibleDenom.end());
+	coefDenom.insert(coefDenom.end(), totalPossibleNum.begin(), totalPossibleNum.end());
+
+        //LOG_STR("Coeff i=" << i << " coef = " << coef);
+	double calcResult = calc(coefNum, coefDenom);
+
+	avg += (calcResult * (calculateAverage(C, N, taken+i) + 1) );
     }
 
     //Special treatment for the i=0 case as its when the current state is repeated
     
-LOG(denom);
+    LOG_STR("denom2 C= " << C << " N= " << N << " Taken " << taken);
+
 LOG(avg);
 LOG(taken);
 
-    double zeroCoef = ( 1.0 * nCk(taken, N) / denom );
-    avg += zeroCoef;
+	vector<int> coefNum;
+	vector<int> coefDenom;
+        nCkBigNumbers(taken, N, coefNum, coefDenom);
 
-LOG(zeroCoef);
+	coefNum.insert(coefNum.end(), totalPossibleDenom.begin(), totalPossibleDenom.end());
+	coefDenom.insert(coefDenom.end(), totalPossibleNum.begin(), totalPossibleNum.end());
+
+	    double zeroCoef = calc( coefNum, coefDenom );
+	    avg += zeroCoef;
+
+	LOG(zeroCoef);
 
     assert(zeroCoef < 1);
     avg /= (1 - zeroCoef);
 
-
+calcAverageResults[index] = avg;
     return avg;
 }
 
 void do_test_case(const int test_case, ifstream& input)
 {
     SHOW_TIME_BEGIN(tc);
-    //LOG_ON();
-    //LOG_OFF();
+    LOG_ON();
+    LOG_OFF();
     
     int C, N;
     input >> C >> N;
     
-    vector<int> trials;
-    VecInt combin;
-    gen_combinations(combin, N, C);
-    
-    const int NUM_TRIALS = 1;
-    
-    LOG_OFF();
-    
-    for(int i = 0; i < NUM_TRIALS; ++i) {
-        vector<bool> found;
-        found.assign(C, false);
-        int numPacks = 0;
-        
-        while(find(found.begin(), found.end(), false) != found.end()) {
-            numPacks ++;
-            int combinNum = rand() % combin.size();
-            LOG(combinNum);
-            
-            int packCards = combin[combinNum];
-            LOG(packCards);
-            for(int c = 0; c < C; ++c) {
-                if ( ( (packCards >> c) & 1 ) == 1) {
-                    LOG_STR("Found " << c << " in " << packCards);
-                    found[c] = true;   
-                }
-            }
-        }
-        
-        LOG(numPacks);
-        trials.push_back(numPacks);
-    }
-    
-    int total =  accumulate(trials.begin(), trials.end(), 0);
-    double average = (1.0 * total) / NUM_TRIALS;
-
-//    cout << "Case #" << (1+test_case) << ": " << average << endl;
-
-    average = calculateAverage(C, N, 0);
+    double average = calculateAverage(C, N, 0);
     
     cout << "Case #" << (1+test_case) << ": " << average << endl;
 }
