da71654a34bdc93fb5d973acdd05d7d0db355def
Eric Gron <eric7237cire@gmail.com>
2012-09-21 19:29:05 +0200

    small

--- 2009/1B/next-number/Main.java
+++ 2009/1B/next-number/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
@@ -10,8 +11,11 @@ import java.util.TreeSet;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multiset;
+import com.google.common.collect.Ordering;
 
 public class Main {
     static class NumCom implements Comparator<String> {
@@ -45,25 +49,12 @@ public class Main {
         }
     }
     
-    private static boolean strictlyDesc(String s) {
-        if (s.length() <= 1) {
-            return true;
-        }
-        
-        for(int si = 0; si < s.length() - 1; ++si) {
-            
-            if (Character.digit(s.charAt(si), 10) < Character.digit(s.charAt(si+1), 10)) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
     
     public static void main(String args[]) throws Exception {
         
-        boolean debug = true; 
+        boolean debug = false; 
         
+        PrintStream sos = System.out;
         PrintStream os = System.out;
         
         if (!debug) {
@@ -71,7 +62,8 @@ public class Main {
         }
         
         
-        Scanner scanner = new Scanner(new File(debug ? "input-sam.txt" : "B-small-practice.in"));
+        Scanner scanner = new Scanner(new File(debug ? "sample.txt" : "B-small-practice.in"));
+        
         int t = scanner.nextInt();
         
         for(int i = 1; i <= t; ++i) {
@@ -80,24 +72,63 @@ public class Main {
             
             List<Integer> listDigits  = new ArrayList<Integer>();
             
+            listDigits.add(0);
             
             for(int si = 0; si < nStr.length(); ++si) {
                 listDigits.add(Character.digit(nStr.charAt(si), 10));
             }
             
             //Find right side group that is not descending
+            Ordering<Integer> intRevOrder = Ordering.natural().reverse();
+            Ordering<Integer> intOrder = Ordering.natural();
             
-            for(int leftIndex = listDigits.size() - 2; leftIndex > 0; ++ leftIndex) {
+            for(int leftIndex = 1; leftIndex < listDigits.size() ; ++ leftIndex) {
+                List<Integer> subList = listDigits.subList(leftIndex, listDigits.size());
                 
+                //os.print(subList);
+                if (intRevOrder.isOrdered(subList)) {
+                	//sos.println("yeah");
+                	
+                	final int nextDigitValue = listDigits.get(leftIndex - 1);
+                	
+                	Iterable<Integer> candidates = Iterables.filter(subList, new Predicate<Integer>() {
+                		 public boolean apply(Integer input) {
+                			 return input > nextDigitValue;
+                		 }
+                	});
+                	
+                	Integer swapValue = intOrder.min(candidates);
+                	int swapIndex = listDigits.lastIndexOf(swapValue);
+                	
+                	Collections.swap(listDigits, swapIndex, leftIndex - 1);
+                	//sos.print(listDigits);
+                	//sos.print(subList);
+                	Collections.sort(subList);
+                	
+                	if (listDigits.get(0) == 0) {
+                		listDigits.remove(0);
+                	}
+                	
+                	String s = StringUtils.join(listDigits, "");
+                	//os.print(s);
+                	
+                	os.format("Case #%d: %s\n", i, s);
+                	break;
+                } else {
+                	//os.println("no");
+                }
             }
             
+            if (1==1) {
+            	continue;
+            }
             NumCom numCom = new NumCom();
             
             //gen all combinations
             SortedSet<String> comb = new TreeSet<String>(numCom);
             
             List<Node> nodes = new ArrayList<Node>();
-            nodes.add(new Node(remaining));
+            //nodes.add(new Node(remaining));
             
             while(nodes.size() > 0) {
                 Node n = nodes.remove(0);
