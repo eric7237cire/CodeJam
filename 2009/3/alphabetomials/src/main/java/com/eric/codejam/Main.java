22fb2f1124d108347046ce58c503f773ab628ad4
Eric Gron <eric7237cire@gmail.com>
2012-10-28 19:29:51 +0100

    alphabetomials

---
+++ 2009/3/alphabetomials/src/main/java/com/eric/codejam/Main.java
@@ -0,0 +1,128 @@
+package com.eric.codejam;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.math3.genetics.PermutationChromosome;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.eric.codejam.utils.Permutations;
+
+public class Main {
+
+    final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    String polynomial;
+    int k;
+    int d;
+    List<String> dictWords;
+
+    public static void handleCase(int caseNumber, Scanner scanner,
+            PrintStream os) {
+
+        Main m = Main.buildMain(scanner);
+
+        List<Integer> total = m.doPerms();
+
+        log.info("Starting case {}", caseNumber);
+
+        os.println("Case #" + caseNumber + ": " + total);
+
+    }
+
+    public List<Integer> doPerms() {
+        List<Integer> totals = new ArrayList<>();
+
+        for (int eachK = 1; eachK <= k; ++eachK) {
+            Integer[] dictArray = new Integer[d];
+            Integer[] combin = new Integer[eachK];
+            int total = 0;
+            for (int i = 0; i < d; ++i) {
+                dictArray[i] = i;
+            }
+
+            Permutations<Integer> perm = Permutations.create(dictArray, combin,
+                    eachK);
+
+            while (perm.next()) {
+                
+                String word = "";
+                for(Integer comIndex : combin) {
+                    word += dictWords.get(comIndex) + " ";
+                }
+                
+                total += evalP(word);
+                
+                log.info("Perm {} word {} total {}", (Object) combin, word, total);
+            }
+            
+            totals.add(total);
+        }
+
+        return totals;
+    }
+
+    public int evalP(String word) {
+        int total = 0;
+        int term = 1;
+        for (int c = 0; c < polynomial.length(); ++c) {
+            char ch = polynomial.charAt(c);
+            if (ch == '+') {
+                total += term;
+                term = 1;
+                continue;
+            }
+            int count = StringUtils.countMatches(word, "" + ch);
+            term *= count;
+        }
+
+        total += term;
+        return total;
+    }
+
+    private static Main buildMain(Scanner scanner) {
+        Main m = new Main();
+
+        m.polynomial = scanner.next();
+        m.k = scanner.nextInt();
+        m.d = scanner.nextInt();
+        m.dictWords = new ArrayList<>();
+        for (int i = 0; i < m.d; ++i) {
+            m.dictWords.add(scanner.next());
+        }
+
+        return m;
+    }
+
+    public Main() {
+
+        // TODO Add test case vars
+        super();
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        if (args.length < 1) {
+            args = new String[] { "sample.txt" };
+        }
+        log.info("Input file {}", args[0]);
+
+        Scanner scanner = new Scanner(new File(args[0]));
+
+        int t = scanner.nextInt();
+
+        for (int i = 1; i <= t; ++i) {
+
+            handleCase(i, scanner, System.out);
+
+        }
+
+        scanner.close();
+    }
+}
---
+++ 2009/3/alphabetomials/src/test/java/com/eric/codejam/AppTest.java
@@ -0,0 +1,132 @@
+package com.eric.codejam;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    final static Logger log = LoggerFactory.getLogger(AppTest.class);
+
+    /**
+     * Create the test case
+     * 
+     * @param testName
+     *            name of the test case
+     */
+    public AppTest() {
+        super();
+    }
+
+    private String getOutput(String testCase) {
+        Scanner sc = new Scanner(testCase);
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream pos = new PrintStream(os);
+
+        Main.handleCase(1, sc, pos);
+
+        try {
+            String output = new String(os.toString("UTF-8"));
+            log.info(output);
+            return output.trim();
+        } catch (UnsupportedEncodingException ex) {
+            return null;
+        }
+    }
+
+    private static String extractAns(String str) {
+        Pattern p = Pattern.compile("Case #\\d: (.*)");
+        Matcher m = p.matcher(str);
+        if (m.matches()) {
+            return m.group(1);
+        }
+
+        return "Error";
+    }
+
+    private static Map<String, String> testData;
+
+    @BeforeClass
+    public static void getTestData() {
+        testData = new HashMap<>();
+
+        try {
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(ClassLoader
+                    .getSystemResourceAsStream("test-data.xml"));
+            doc.getDocumentElement().normalize();
+
+            NodeList nl = doc.getElementsByTagName("test");
+            for (int i = 0; i < nl.getLength(); ++i) {
+                Node n = nl.item(i);
+                String value = n.getTextContent();
+
+                String name = n.getAttributes().getNamedItem("name")
+                        .getNodeValue();
+
+                testData.put(name, value);
+            }
+        } catch (Exception ex) {
+
+        }
+
+    }
+
+    @Test
+    public void testS1() {
+        String testCase = testData.get("s1");
+
+        // double output = Double.parseDouble(extractAns(getOutput(testCase)));
+
+        // assertEquals(7, output, DoubleComparator.TOLERANCE);
+    }
+
+    @Test
+    public void testS2() {
+        String testCase = testData.get("s2");
+
+        String output = extractAns(getOutput(testCase));
+
+        assertEquals("12 96 576", output);
+    }
+
+    @Test
+    public void testCount() {
+        Main m = new Main();
+        m.polynomial = "a+e";
+        int count = m.evalP("ejoe");
+        assertEquals(2, count);
+
+        m.polynomial = "aber+aab+c";
+        count = m.evalP("abracadabra edgar");
+        assertEquals(109, count);
+    }
+
+}
