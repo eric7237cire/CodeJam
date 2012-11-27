56ecb881f32a222dae7fcfae0fa77e760790e044
Eric Gron <eric7237cire@gmail.com>
2012-11-27 19:38:01 +0100

    Very fast large

--- 2010/3/rng/src/main/java/com/eric/codejam/Main.java
+++ 2010/3/rng/src/main/java/com/eric/codejam/Main.java
@@ -2,11 +2,15 @@ package com.eric.codejam;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
@@ -17,7 +21,10 @@ import com.eric.codejam.multithread.Consumer.TestCaseHandler;
 import com.eric.codejam.multithread.Producer.TestCaseInputReader;
 import com.eric.codejam.utils.Prime;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
 import com.google.common.math.IntMath;
+import com.google.common.math.LongMath;
+import com.google.common.primitives.Ints;
 
 public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<InputData> {
 
@@ -65,98 +72,151 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
         return ambig;
     }
     
+    public long determinant(int S_1, int S_2, int S_3, int S_4) {
+        //Three points (S_1, S_2) (S2, S3) and (S3,S4)
+        
+        //Normalize points, make p1 origin
+        int p1_x = S_1;
+        int p1_y = S_2;
+        
+        int p2_x = S_2 - p1_x;
+        int p2_y = S_3 - p1_y;
+        
+        int p3_x = S_3 - p1_x;
+        int p3_y = S_4 - p1_y;
+        
+        //Det ab cd
+        long a = p2_x;
+        long b = p2_y;
+        long c = p3_x;
+        long d = p3_y;
+        return Math.abs(LongMath.checkedMultiply(a,d)-LongMath.checkedMultiply(b,c));
+    }
+    
+    public int[] getAandBGivenP(int S1, int S2, int S3, int P) {
+        
+        //Slope is normally change Y / change X.  But we must use mod math
+        
+        long s3_s2 = (S3 - S2) % P;
+        int s2_s1 = (S2 - S1) % P;
+        
+        BigInteger bi = BigInteger.valueOf(s2_s1);
+        BigInteger mi = bi.modInverse(BigInteger.valueOf(P));
+        
+        int a = Ints.checkedCast(( LongMath.checkedMultiply(s3_s2, mi.intValue())) % P);
+        if (a < 0) 
+            a+=P;
+        int b = (S2 - Ints.checkedCast(LongMath.checkedMultiply(a, S1) % P)) % P;
+        if (b < 0)
+            b+=P;
+        
+        return new int[] {a, b};
+    }
+    
+    public int getNext(int S1, int S2, int S3, int S_last, int P) {
+        int[] a_b = getAandBGivenP(S1, S2, S3, P);
+        long a = a_b[0];
+        long b = a_b[1];
+        int n = Ints.checkedCast((a * S_last + b) % P);
+       // log.debug("a {} b {} p {} produces {}", a,b,prime,n);
+        Preconditions.checkState(n >= 0);
+        return n;
+    }
+    
+    static List<SortedSet<Integer>> allPrimes = new ArrayList<>(6);
+    
+    static {
+        for(int d = 1; d <= 6; ++d)
+        allPrimes.add(new TreeSet<>(Prime.generatePrimes(IntMath.pow(10,d))));
+    }
+    
+    public Set<Integer> getPrimeFactorsUsingParraleogam(SortedSet<Integer> primes, InputData input, int maxSeed) {
+        /*
+         * The area of a parralelogram contained by three points must be a multiple of p.
+         * 
+         * All points (S_i, S_i+1) fall on lines given by
+         * y=ax+b
+         * y=ax+b-p
+         * y=ax+b-2p
+         * y=ax+b-3p
+         * 
+         * Thus the parrelogram of points from these lines must have an area P*N
+         */
+    
+        Set<Integer> allPrimeFactors = new HashSet<>();
+
+        for (int k = 0; k <= input.seedOutput.length - 4; ++k) {
+            long det = determinant(input.seedOutput[k],
+                    input.seedOutput[k + 1], input.seedOutput[k + 2],
+                    input.seedOutput[k + 3]);
+            Set<Integer> primeFactors = new HashSet<>();
+            //log.debug("Det is {}", det);
+            
+            if (det == 0) {
+                primeFactors = primes;
+            } else {
+                for (int p : primes) {
+                    Preconditions.checkArgument(p > maxSeed);
+
+                    if (det % p == 0) {
+                        primeFactors.add(p);
+                    }
+                }
+            }
+
+            //log.debug("Prime factors for k {} = {}", k, primeFactors);
+            if (k == 0) {
+                allPrimeFactors = primeFactors;
+            } else {
+                allPrimeFactors = Sets.intersection(allPrimeFactors,
+                        primeFactors);
+            }
+        }
+
+        return allPrimeFactors;
+    }
+    
     @Override
     public String handleCase(int caseNumber, InputData input) {
 
-        log.info("Starting calculating case {}", caseNumber);
+        //log.info( "case {}.  Max prime {}  sequence {}", caseNumber, IntMath.pow(10,input.D), input.seedOutput);
         
-        List<Integer> primes = Prime.generatePrimes(IntMath.pow(10,input.D));
+        SortedSet<Integer> primes = allPrimes.get(input.D - 1);
         
-        boolean  ambig = false; 
         int nextNumber = -1;
         
         int maxSeed = Collections.max(Arrays.asList(ArrayUtils.toObject(input.seedOutput)));
         
-        if (input.K == 1) {
-            if (input.seedOutput[0] == 0) {
-                //ambig = false;
-                //nextNumber = 0;
-                ambig=true;
-            } else {
-                ambig = true;
-            }
-        } else {
+        if (input.K >= 2 && input.seedOutput[0] == input.seedOutput[1]) {
+            nextNumber = input.seedOutput[0];
+        } else if (input.K >= 3) {
         
-        outerLoop:
-        for(int p : primes) {
-            if (p <= maxSeed)
-                continue;
-            for(int a = 0; a < p; ++a) {
-                
-                //Calculate a(s1) mod p
-                int a_s = a * input.seedOutput[0] % p;
-                
-                Preconditions.checkState(a_s >= 0 && a_s < p);
-                // a(s1)%p + b%p <= 2P - 2
-                // a(s1)%p + b%p = s2 or s2 + p
-                int b1 = input.seedOutput[1] - a_s;
-                int b2 = input.seedOutput[1]+p - a_s;
-                int b;
-                if (0 <= b1 && b1 < p) {
-                    Preconditions.checkState(!(0 <= b2 && b2 < p));
-                    b = b1; 
-                } else if (0 <= b2 && b2 < p){
-                    Preconditions.checkState(!(0 <= b1 && b1 < p));
-                    b=b2;
-                } else {
+            Set<Integer> primesToCheck = input.K >= 4 ? getPrimeFactorsUsingParraleogam(primes.tailSet(maxSeed+1),input,maxSeed) : primes.tailSet(maxSeed+1);
+                                
+            for (int prime : primesToCheck) {                
+                int n = getNext(input.seedOutput[0],
+                        input.seedOutput[1], input.seedOutput[2], input.seedOutput[input.seedOutput.length - 1], prime);
+                                                
+                if (nextNumber != -1 && nextNumber != n) {
+                    nextNumber = -1;
                     break;
+                } else {
+                    nextNumber = n;
                 }
-                Preconditions.checkArgument(b >= 0 && b < p);
-                
-                Preconditions.checkState( (a * input.seedOutput[0] + b) % p == input.seedOutput[1]);
-                        
-                //for(int b = 0; b < p; ++b) {
-                    boolean valid=true;
-                    for(int s = 1; s < input.K; ++s) {
-                        if ( (a * input.seedOutput[s-1] + b) % p != input.seedOutput[s])
-                        {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    if (valid) {
-                        int thisNextNum = (a * input.seedOutput[input.seedOutput.length-1] + b) % p; 
-                        if (nextNumber == -1) {
-                            log.debug("First Match a {} b {} p {}  numbers next {}", a, b, p, thisNextNum);
-                            nextNumber = thisNextNum;
-                        } else if (nextNumber != thisNextNum){
-                            ambig = true;
-                            log.debug("Second Match a {} b {} p {}  numbers next {}", a, b, p, thisNextNum);
-                            break outerLoop;
-                        }
-                        //log.debug("Match a {} b {} p {}", a, b, p);
-                        
-                    }
-               // }
-                
             }
-        }
-        
-        }
+        } 
 
-        if (input.D <= 2) {
-//        boolean check = bruteForce(input);
+      //   log.info("Done calculating answer case {}", caseNumber);
         
-  //      Preconditions.checkState(check == ambig);
-        }
-         log.info("Done calculating answer case {}", caseNumber);
-        
-        //DecimalFormat decim = new DecimalFormat("0.00000000000");
-        //decim.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
-        if (!ambig)
-        return ("Case #" + caseNumber + ": " + nextNumber);
-        else {
-            return ("Case #" + caseNumber + ": I don't know");
+        if (nextNumber >= 0) {
+            log.info("case {}.  Max prime {}  sequence {}", caseNumber,
+                    IntMath.pow(10, input.D), input.seedOutput);
+            return ("Case #" + caseNumber + ": " + nextNumber);
+        } else {
+            log.info("case {}.  Max prime {}  sequence {}", caseNumber,
+                    IntMath.pow(10, input.D), input.seedOutput);
+
+            return ("Case #" + caseNumber + ": I don't know.");
         }
     }
     
@@ -194,9 +254,9 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
     public static void main(String args[]) throws Exception {
 
         if (args.length < 1) {
-            args = new String[] { "sample.txt" };
-            //args = new String[] { "A-small-practice.in" };
-//            args = new String[] { "B-large-practice.in" };
+           // args = new String[] { "sample.txt" };
+         //   args = new String[] { "A-small-practice.in" };
+            args = new String[] { "A-large-practice.in" };
          }
          log.info("Input file {}", args[0]);
 
