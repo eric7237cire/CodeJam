d545a6435af2302790a160cdcf64244c0592a04f
Eric Gron <eric7237cire@gmail.com>
2012-11-30 23:16:43 +0100

    closer

---
+++ 2010/3/hotdog/genSample.rb
@@ -0,0 +1,5 @@
+puts 200
+(1..200).each do |num|
+puts 1
+puts "0 #{num}"
+end
--- 2010/3/hotdog/src/main/java/com/eric/codejam/Main.java
+++ 2010/3/hotdog/src/main/java/com/eric/codejam/Main.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +22,14 @@ import com.eric.codejam.main.Runner;
 import com.eric.codejam.multithread.Consumer.TestCaseHandler;
 import com.eric.codejam.multithread.Producer.TestCaseInputReader;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.TreeMultiset;
 
 public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<InputData> {
 
     final static Logger log = LoggerFactory.getLogger(Main.class);
+    
+    static CircularFifoBuffer lastElems = new CircularFifoBuffer(3);
     
     @Override
     public String handleCase(int caseNumber, InputData input) {
@@ -36,17 +38,28 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
         
         //double ans = DivideConq.findMinPerimTriangle(input.points);
 
-        //int cost = doBreadthFirstSearch(input.vendors);
-        int cost = 0;
+        long cost = 0;
+        //cost = doBreadthFirstSearch(input.vendors);
         
-        int cost2 = doBreadthFirstSearch2(input.vendors);
         
-        log.debug("Case #" + caseNumber + ": " + cost + ", " + cost2);
+        //int cost2 = takeFirstMove(input.vendors);
+        
+        cost = takeAllMove(input.vendors);
+    
+        /*lastElems.add(cost2);
+        
+        int lastEl = 0;
+        Iterator<Integer> it = lastElems.iterator();
+       // while(it.hasNext()) {
+            lastEl = (Integer) it.next();
+        //}*/
+        
+        //log.debug("Case {} cost {} diff {}",  caseNumber, cost2, cost2 - lastEl);
         
         //DecimalFormat decim = new DecimalFormat("0.00000000000");
         //decim.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
         
-        return ("Case #" + caseNumber + ": " +  cost2);
+        return ("Case #" + caseNumber + ": " +  cost);
     }
     
     @Override
@@ -158,7 +171,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
                 return currentNode.cost;
             }
             
-            for(int i = 0; i < possibleMoves.size(); ++i) {
+            for(int i = 0; i < 1; ++i) {
                 int corner = possibleMoves.get(i);
                 Multiset<Integer> copy = TreeMultiset.create(currentVendors);
                 
@@ -179,21 +192,80 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
         
     }
 
+ public long takeAllMove(List<Corner> vendors) {
+                
+        
+        Multiset<Integer> currentVendors = TreeMultiset.create();
+        LinkedList<Integer> toProcess = new LinkedList<Integer>();
+        
+        for(int i = 0; i < vendors.size(); ++i) {
+            currentVendors.add(vendors.get(i).location, vendors.get(i).count);
+            if (vendors.get(i).count > 1) {
+                toProcess.add(vendors.get(i).location);
+            }
+        }
+        long cost = 0;
+        
+        int iterations = 0;
+        
+        while(!toProcess.isEmpty()) {
+            
+            
+           
+           
+            Integer corner = toProcess.pollFirst();
+            
+            
+            int count = currentVendors.count(corner);
+            if (count <= 1)
+                continue;
+            
+            ++iterations;
+            if (iterations % 1000000 == 0) 
+                log.debug("Iterations {}.  Size {}  corner count {}", iterations/100000, currentVendors.elementSet().size(), count);
+            
+            int half = count / 2;
+            long costAdd = (long)half*(half+1)*(2*half+1)/6;
+            cost+=costAdd;
+            
+            for(int leftSide = corner - half; leftSide < corner; ++leftSide) {                
+                int beforeCount = currentVendors.count(leftSide);
+                beforeCount ++;
+                if (beforeCount > 1)
+                    toProcess.add(leftSide);
+                currentVendors.setCount(leftSide,beforeCount);
+            }
+            for(int rightSide = corner + half; rightSide > corner; --rightSide) {                
+                int beforeCount = currentVendors.count(rightSide);
+                beforeCount ++;
+                if (beforeCount > 1)
+                    toProcess.add(rightSide);
+                currentVendors.setCount(rightSide,beforeCount);
+            }
+            
+            if (count % 2 == 0) {
+                currentVendors.setCount(corner, 0);
+            } else {
+                currentVendors.setCount(corner, 1);
+            }
+            
+            if (cost % 1000000 == 0) {
+                log.debug("Cost is {}", cost);
+            }
+            
+            
+        }
+        
+        return cost;
+    }
 
     /*
      * Finds shortest distance to a target leftover.
      * 
      * 
      */
-    public Integer doBreadthFirstSearch2(List<Corner> vendors) {
-        
-        
-        Queue<Node> toProcess = new PriorityQueue<>(1, new Comparator<Node>() {
-            public int compare(Node a, Node b) {
-                return Integer.compare(a.cost, b.cost);
-            }
-        });
-        
+    public Integer takeFirstMove(List<Corner> vendors) {
+                
         
         Multiset<Integer> vendorCounts = TreeMultiset.create();
         for(int i = 0; i < vendors.size(); ++i) {
@@ -229,6 +301,9 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
             }
         
             ++cost;
+            if (cost % 100000 == 0) {
+                log.debug("Cost is {}", cost);
+            }
             currentVendors.setCount(possibleMove, currentVendors.count(possibleMove) - 2);
             currentVendors.add(possibleMove-1);
             currentVendors.add(possibleMove+1);
@@ -252,7 +327,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputReader<Inp
 
         if (args.length < 1) {
             args = new String[] { "sample.txt" };
-           // args = new String[] { "B-small-practice.in" };
+           // args = new String[] { "C-small-practice.in" };
 //            args = new String[] { "B-large-practice.in" };
          }
          log.info("Input file {}", args[0]);
