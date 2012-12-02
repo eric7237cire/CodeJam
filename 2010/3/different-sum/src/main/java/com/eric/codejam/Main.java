8152a0ccf93f2b7ddf44c3dc666f8573393f6681
Eric Gron <eric7237cire@gmail.com>
2012-12-02 15:10:20 +0100

    term / sum count

--- 2010/3/different-sum/src/main/java/com/eric/codejam/Main.java
+++ 2010/3/different-sum/src/main/java/com/eric/codejam/Main.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import com.eric.codejam.main.Runner;
 import com.eric.codejam.main.Runner.TestCaseInputScanner;
 import com.eric.codejam.multithread.Consumer.TestCaseHandler;
+import com.google.common.base.Preconditions;
 
 public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<InputData> {
 
@@ -29,27 +30,32 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         
         boolean[][] fd = new boolean[s.length()][base];
         
-        int count = count(n,n, base,fd, new ArrayList<String>());
+        int[] counts = count(n,n, base,fd, new ArrayList<String>());
+        
+        int termCount = counts[1];
+        int sumCount = counts[0];
+        
+        log.debug("term count {} sum count {}", termCount, sumCount);
         for(int i = 0; i < s.length(); ++i){
             //Arrays.fill(fd[i], -1);
         }
         //DecimalFormat decim = new DecimalFormat("0.00000000000");
         //decim.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
         
-        return ("Case #" + caseNumber + ": " + count);
+        return ("Case #" + caseNumber + ": " + sumCount);
     }
     
-    public int count(long n, long maxNum, final int b, boolean[][] forbiddenDigits, List<String> prevNums) {
-        int count = 0;
+    public int[] count(long n, long maxNum, final int b, boolean[][] forbiddenDigits, List<String> prevNums) {
+        int[] count = new int[] {0,0};
         
         if (n == 0) {
-           // log.debug("Prev nums\n{}", StringUtils.join(prevNums, "\n"));
+            log.debug("Prev nums\n{}", StringUtils.join(prevNums, "\n"));
             
-            return 1;
+            return new int[] {1, prevNums.size()};
         }
         
         if (n < 0)
-            return 0;
+            return new int[] {0,0};
         
         topLoop: for (long topNum = 1; topNum <= maxNum; ++topNum) {
             String s = StringUtils.leftPad(Long.toString(topNum, b),
@@ -70,7 +76,9 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
                 forbiddenDigits[c][Character.digit(s.charAt(c), b)] = true;
             }
             prevNums.add(s);
-            count = count + count(n - topNum, topNum, b, forbiddenDigits, prevNums);
+            int[] subCounts = count(n - topNum, topNum, b, forbiddenDigits, prevNums);
+            count[0] += subCounts[0];
+            count[1] += subCounts[1];
             prevNums.remove(s);
             for (int c = 0; c < s.length(); ++c) {
                 if (!Character.isDigit(s.charAt(c)))
@@ -84,7 +92,45 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
     }
     
     
-    
+    public int[] getSumTermCount(int maxColumn, int nextColumnDigit, int columnDigit, int maxDigit, final int base) {
+        
+        int[] count = new int[] {0,0};
+        if (maxDigit == 0) {
+            return count;
+        }
+        Preconditions.checkArgument(maxDigit > 0 && maxDigit <= 70);
+        
+        //in base 10
+        int maxN = nextColumnDigit * base + columnDigit;
+        
+        for(int digit = maxDigit; digit >= 1; --digit) 
+        {
+            int rest = maxN - digit;
+            
+            if(rest < 0)
+                continue;
+            
+            if (rest == 0) {
+                count[0] ++;
+                count[1] ++;
+                continue;
+            }
+            
+            int subNextColDigit = rest / base;
+            int subColDigit = rest % base;
+            
+            int[] subCount = getSumTermCount(maxColumn,subNextColDigit, subColDigit, digit - 1, base);
+            
+            if(subCount[1] == 0)
+                continue;
+            
+            count[0]+=subCount[0];
+            count[1]+= subCount[0]+subCount[1];
+            
+        }
+        
+        return count;
+    }
 
     @Override
     public InputData readInput(Scanner scanner, int testCase) {
--- 2010/3/different-sum/src/test/java/com/eric/codejam/AppTest.java
+++ 2010/3/different-sum/src/test/java/com/eric/codejam/AppTest.java
@@ -1,13 +1,8 @@
 package com.eric.codejam;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
-
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.eric.codejam.test.TesterBase;
 
 import static org.junit.Assert.*;
 
@@ -29,7 +24,50 @@ public class AppTest  {
     }
 
     
-
+    @Test
+    public void testSumTermCount() {
+        Main m = new Main();
+        
+        //public int[] getSumTermCount(int maxColumn, int nextColumnDigit, int columnDigit, int maxDigit, final int base) {
+        
+        final int BASE = 10;
+        
+        int[] sumTermCount = m.getSumTermCount(1,0,1,9,BASE);
+        assertEquals(1, sumTermCount[0]); //sum count
+        assertEquals(1, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,2,2,BASE);
+        assertEquals(1, sumTermCount[0]); //sum count
+        assertEquals(1, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,3,9,BASE);
+        assertEquals(2, sumTermCount[0]); //sum count
+        assertEquals(3, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,4,9,BASE);
+        assertEquals(2, sumTermCount[0]); //sum count
+        assertEquals(3, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,5,9,BASE);
+        assertEquals(3, sumTermCount[0]); //sum count
+        assertEquals(5, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,6,9,BASE);
+        assertEquals(4, sumTermCount[0]); //sum count
+        assertEquals(8, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,7,7,BASE);
+        assertEquals(5, sumTermCount[0]); //sum count
+        assertEquals(10, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,0,8,9,BASE);
+        assertEquals(6, sumTermCount[0]); //sum count
+        assertEquals(13, sumTermCount[1]); //term count
+        
+        sumTermCount = m.getSumTermCount(1,4,5,9,BASE);
+        assertEquals(1, sumTermCount[0]); //sum count
+        assertEquals(9, sumTermCount[1]); //term count
+    }
     
 
 }
