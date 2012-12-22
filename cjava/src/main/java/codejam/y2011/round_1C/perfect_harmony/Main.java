3981910836b3963e199ae4ab0dcc0633836f6108
Eric Gron <eric7237cire@gmail.com>
2012-12-22 14:32:07 +0100

    Improve solution using answer

--- cjava/src/main/java/codejam/y2011/round_1C/perfect_harmony/Main.java
+++ cjava/src/main/java/codejam/y2011/round_1C/perfect_harmony/Main.java
@@ -2,20 +2,22 @@ package codejam.y2011.round_1C.perfect_harmony;
 
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.math.BigIntegerMath;
-import com.google.common.math.LongMath;
-
 import codejam.utils.main.DefaultInputFiles;
 import codejam.utils.main.Runner.TestCaseInputScanner;
 import codejam.utils.multithread.Consumer.TestCaseHandler;
-import codejam.utils.utils.Direction;
-import codejam.utils.utils.GridChar;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Ordering;
+import com.google.common.math.LongMath;
 
 public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<InputData>, DefaultInputFiles{
 
@@ -23,8 +25,8 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
     
     @Override
     public String[] getDefaultInputFiles() {
-       return new String[] {"sample.in"};
-       // return new String[] {"B-small-practice.in", "B-large-practice.in"};
+       //return new String[] {"sample.in"};
+        return new String[] {"C-small-practice.in", "C-large-practice.in"};
     }
     
     @Override
@@ -43,6 +45,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         return in;
     }
     
+    
     public boolean isPrime (long n)
     {
        if (n<=1) return false;
@@ -50,11 +53,30 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
        if (n%2==0) return false;
        long m=LongMath.sqrt(n, RoundingMode.UP);
 
-       for (long i=3; i<=m; i+=2)
+       //100 000 000 / 2 iterations is OK!
+       for (long i=3; i<=m; i+=2) {
+           
           if (n%i==0)
              return false;
+       }
 
        return true;
+    }
+    
+    public List<Long> getDivisors(long n) {
+        ArrayList<Long> ret = new ArrayList<Long>();
+        long m=LongMath.sqrt(n, RoundingMode.UP);
+        
+        for(long d = 1; d <= m; ++d) {
+            if (n%d == 0) {
+                ret.add(d);
+                ret.add(n / d);
+            }
+        }
+        
+        Collections.sort(ret, Ordering.natural().reverse());
+        
+        return ret;
     }
     
     @Override
@@ -62,9 +84,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
      
         //our frequency has to be <= anothers frequency
         
-        BigInteger[] gcdDec = new BigInteger[in.N];
-        //long[] gcdDes = new long[in.N];
-        //BigInteger[] prodDes = new BigInteger[in.N];
+        long[] gcdDec = new long[in.N];
         BigInteger[] lcmAsc  = new BigInteger[in.N];
         
         Arrays.sort(in.freq);
@@ -76,16 +96,15 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
             lcmAsc[freq] = lcmAsc[freq-1].multiply(BigInteger.valueOf(in.freq[freq])).divide(gcd);
         }
         
-        gcdDec[in.N-1] = BigInteger.valueOf(in.freq[in.N - 1]);
+        gcdDec[in.N-1] = in.freq[in.N - 1];
         for(int freq = in.N - 2; freq >= 0; --freq) {
-            gcdDec[freq] = gcdDec[freq+1].gcd(BigInteger.valueOf(in.freq[freq]));
+            gcdDec[freq] = BigInteger.valueOf(gcdDec[freq+1]).gcd(BigInteger.valueOf(in.freq[freq])).longValue();
         }
         
         //Position     0    1   2   3   4
         //Frequencies     0   1   2   3
         log.debug("gcd des: {}", (Object)gcdDec);
-//        log.debug("gcd des: {}", gcdDes);
-//        log.debug("Prod des: {}", (Object) prodDes);
+
         log.debug("LCM asc: {}", (Object) lcmAsc);
         log.debug("Freq: {}", in.freq);
         for(int guessPosition = 0; guessPosition <= in.N; ++guessPosition) {
@@ -96,16 +115,12 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
             BigInteger lcm = guessPosition == 0 ? BigInteger.ONE : lcmAsc[guessPosition-1];
             
             //GCD of all frequences greater
-            BigInteger gcd = guessPosition == in.N ? null : gcdDec[guessPosition];
+            long gcd = guessPosition == in.N ? -1 : gcdDec[guessPosition];
             
             
             log.debug("In position {} lcm {} gcd {}", guessPosition, lcm, gcd);
             
-            if (gcd == null) {
-//                BigInteger low = lcm.divide(BigInteger.valueOf(in.L));
-//                if (low.equals(BigInteger.ZERO)) {
-//                    return String.format("Case #%d: NO", in.testCase);
-//                }
+            if (gcd == -1) {
                 
                 long lowK1 = BigInteger.valueOf(in.L).divide(lcm).longValue();
                 
@@ -114,7 +129,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
                 
                 long uppK1 = LongMath.divide(in.H, lcm.longValue(), RoundingMode.UP);
                 
-                log.info("Lower bound k1 {} upp {} ", lowK1, uppK1);
+                log.debug("Lower bound k1 {} upp {} ", lowK1, uppK1);
                 
                 for(long k1 = lowK1; k1 <= uppK1; ++k1) {
                     
@@ -125,88 +140,48 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
                     }
                 }
 
-//                BigInteger ans = low.multiply(lcm);
-//
-//                if (BigInteger.valueOf(in.L).compareTo(ans) >= 0 && BigInteger.valueOf(in.H).compareTo(ans) <= 0) {
-//                    return String.format("Case #%d: %s", in.testCase, ans.toString());
-//                }
-
             } else {
                 
-                if (BigInteger.valueOf(in.L).compareTo(gcd) > 0)
+                if (in.L > gcd)
                     continue; 
                 
                 if (BigInteger.valueOf(in.H).compareTo(lcm) < 0) 
                     continue;
                 
-                BigInteger[] div = gcd.divideAndRemainder(lcm);
+                BigInteger[] div = BigInteger.valueOf(gcd).divideAndRemainder(lcm);
+                
                 if (!div[1].equals(BigInteger.ZERO))
                     continue;
-
-                log.info("In position {} lcm {} gcd {}.  div = {}", guessPosition, lcm, gcd, div[0]);
-
+               
                 //P = k1 * lcm
                 //P = gcd / k2
                 //C = k1 * k2
                 
-                long lowK1 = BigInteger.valueOf(in.L).divide(lcm).longValue();
-                long lowK2 = LongMath.divide(gcd.longValue(), in.H, RoundingMode.UP);
                 
-                if (lowK1 == 0)
-                    lowK1 = 1;
+                long uppK2 = LongMath.divide(gcd, in.L, RoundingMode.UP);
                 
-                if (lowK2 == 0)
-                    lowK2 = 1;
+                long C =   div[0].longValue(); //gcd / lcm.longValue();
                 
-                long uppK1 = LongMath.divide(in.H, lcm.longValue(), RoundingMode.UP);
-                long uppK2 = LongMath.divide(gcd.longValue(), in.L, RoundingMode.UP);
-                long C = div[0].longValue();
+                List<Long> k2Divisors = getDivisors(gcd);
                 
-                long tryK2 = uppK2;
-                long tryK1 = lowK1;
-                
-                log.info("Lower bound k2 {} up {}  C {}", lowK2, uppK2, C);
-                log.info("Lower bound k1 {} upp {}  C {}", lowK1, uppK1, C);
-                
-                log.info("Is prime testing");
-                
-                boolean isPrime = isPrime(gcd.longValue());
-                
-                if (isPrime) {
-                    uppK2 = 1;
-                    tryK2 = 1;
-                }
+                log.debug("In position {} lcm {} gcd {}. sq gcd{}  ", guessPosition,
+                        lcm, gcd);
 
-                while (tryK2 >= lowK2 && tryK1 <= uppK1) {
-                    //log.info("Tryk2 {} k1 {}", tryK2, tryK1);
-                    
-                    if (C % tryK2 != 0) {
-                        tryK2--;
-                    } else {
-                        long k1 = C / tryK2;
+                for (long k2 : k2Divisors) {
 
-                        BigInteger ans = lcm.multiply(BigInteger.valueOf(k1));
+                    if (k2 > uppK2)
+                        continue;
 
-                        if (BigInteger.valueOf(in.L).compareTo(ans) <= 0 && BigInteger.valueOf(in.H).compareTo(ans) >= 0) {
-                            return String.format("Case #%d: %s", in.testCase, ans.toString());
-                        }
+                    if (C % k2 != 0)
+                        continue;
 
-                        tryK2--;
-                    }
+                    Preconditions.checkState(C % k2 == 0);
+                    long k1 = C / k2;
 
-                    
-                    if (C % tryK1 != 0) {
-                        tryK1++;
-                    } else {
-                        long k2 = C / tryK1;
+                    BigInteger ans = lcm.multiply(BigInteger.valueOf(k1));
 
-                        BigInteger ans = lcm.multiply(BigInteger.valueOf(tryK1));
-
-                        if (BigInteger.valueOf(in.L).compareTo(ans) <= 0 && BigInteger.valueOf(in.H).compareTo(ans) >= 0) {
-                            return String.format("Case #%d: %s", in.testCase, ans.toString());
-                        }
-                        
-                        tryK1++;
+                    if (BigInteger.valueOf(in.L).compareTo(ans) <= 0 && BigInteger.valueOf(in.H).compareTo(ans) >= 0) {
+                        return String.format("Case #%d: %s", in.testCase, ans.toString());
                     }
 
                 }
