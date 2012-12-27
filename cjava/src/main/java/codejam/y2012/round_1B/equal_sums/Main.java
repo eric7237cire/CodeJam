c361f8b9648fdb4d27e9ec76edc75d78c947bf69
Eric Gron <eric7237cire@gmail.com>
2012-12-27 18:45:35 +0100

    huh large 1b

--- cjava/src/main/java/codejam/y2012/round_1B/equal_sums/Main.java
+++ cjava/src/main/java/codejam/y2012/round_1B/equal_sums/Main.java
@@ -1,12 +1,17 @@
 package codejam.y2012.round_1B.equal_sums;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
@@ -31,6 +36,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
     @Override
     public String[] getDefaultInputFiles() {
          return new String[] {"sample.in"};
+        //return new String[] {"C-small-practice.in"};
       //  return new String[] { "A-small-practice.in", "A-large-practice.in" };
     }
 
@@ -85,10 +91,81 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         
         log.info("none");
     }
+    
+    public Pair<List<Long>, List<Long>> random(InputData in) {
+                
+
+        Map<Long, BitSet> map = Maps.newHashMap();
+        
+        Random r = new Random(0);
+        
+        //10 ^ 12 * 6 = X
+        //500 choose 6 = Y
+        //Since Y > X, pigeon hole says there is a solution
+        
+        //long[] set1 = new long[6]; 
+        int count = 0;
+        long sum  = 0;
+        
+        for (int i = 0; i < 3000000; ++i) {
+         //   log.debug("Set1 {}", set1);
+         //   log.debug("Set2 {}", set2);
+            
+            BitSet bs = new BitSet();
+            count  = 0;
+            sum = 0;
+            while (count < 6) {
+                int bit = r.nextInt(500);
+                
+                if (!bs.get(bit)) {
+                    long n = in.numbers.get(bit);
+                    sum += n;
+                    bs.set(bit);
+                    ++count;
+                }
+            }
+            
+            
+            
+            
+            if (map.containsKey(sum)) {
+                log.info("Found at {}", i);
+                
+                List<Long> existingList = new ArrayList<>();
+                for (int b = bs.nextSetBit(0); b >= 0; b = bs.nextSetBit(b+1)) {
+                    // operate on index i here
+                    existingList.add(in.numbers.get(b));
+                }
+                List<Long> retList = new ArrayList<>();
+                bs = map.get(sum);
+                for (int b = bs.nextSetBit(0); b >= 0; b = bs.nextSetBit(b+1)) {
+                    // operate on index i here
+                    retList.add(in.numbers.get(b));
+                }
+                
+                return new ImmutablePair<>(existingList, retList);
+            }
+
+            map.put(sum, bs);
+
+        }
+
+        return null;
+
+    }
 
     public String handleCase(InputData in) {
 
-        bruteForce(in);
+       // bruteForce(in);
+        
+        if (in.N == 500) {
+            Pair<List<Long>,List<Long>> ans = random(in);
+            if (ans == null)
+                return (String.format("Case #%d:\nImpossible", in.testCase));
+            
+            return (String.format("Case #%d:\n%s\n%s", in.testCase, Joiner.on(' ').join(ans.getLeft()),
+                    Joiner.on(' ').join(ans.getRight()) ));
+        }
         
         TreeMap<Long, Pair<Set<Long>, Set<Long> >> setDiffs = new TreeMap<>();
         
@@ -109,8 +186,11 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
                         (Set<Long>) Sets.<Long>newHashSet(),
                         (Set<Long>) Sets.newHashSet(in.numbers.get(1))));
         
+        long sumRest = sumSet(in.numbers) - in.numbers.get(0) - in.numbers.get(1);
+        
         for(int i = 2; i < in.N; ++i) {
             long num = in.numbers.get(i);
+            sumRest -= num;
             if (setDiffs.containsKey(num)) {
                 Pair<Set<Long>, Set<Long> > sets = setDiffs.get(num);
                 sets.getLeft().add(num);
@@ -118,6 +198,9 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
                         Joiner.on(' ').join(sets.getRight()) ));
             }
             
+            log.debug("Set diff keys size {}", setDiffs.keySet().size());
+            
+            setDiffs.tailMap(sumRest).clear();
             
             TreeMap<Long, Pair<Set<Long>, Set<Long> >> toAdd = new TreeMap<>();
             
