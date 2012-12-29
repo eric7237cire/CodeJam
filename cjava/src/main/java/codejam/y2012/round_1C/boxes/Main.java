fad0fa042ac1430950e9a919244c517f1ea70541
Eric Gron <eric7237cire@gmail.com>
2012-12-29 19:06:32 +0100

    guh

--- cjava/src/main/java/codejam/y2012/round_1C/boxes/Main.java
+++ cjava/src/main/java/codejam/y2012/round_1C/boxes/Main.java
@@ -1,7 +1,11 @@
 package codejam.y2012.round_1C.boxes;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.NavigableSet;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -12,6 +16,7 @@ import codejam.utils.main.DefaultInputFiles;
 import codejam.utils.main.Runner.TestCaseInputScanner;
 import codejam.utils.multithread.Consumer.TestCaseHandler;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
 public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<InputData>, DefaultInputFiles {
@@ -46,8 +51,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         return in;
     }
 
-    public String handleCase(InputData in) {
-
+    public int[][] bruteForce(InputData in) {
         List<Integer> aExpanded = Lists.newArrayList();
         
         for(int n = 0; n < in.N; ++n) {
@@ -73,8 +77,6 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         
         for(int i= 0; i < m; ++i) {
             
-            log.debug("i {}", i);
-            
             for(int j = 0; j < n; ++j) {
                 
                 if (aExpanded.get(i) == bExpanded.get(j)) {
@@ -98,8 +100,354 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
                 lcs[i][j] = max;
             }
         }
+        
+        return lcs;
+
+    }
+    
+    
+    private static class Block {
+        /**
+         * Where the block would start in the LCS array
+         */
+        long startingIndex;
+        
+        /**
+         * How many items
+         */
+        long count;
+        
+        /**
+         * Toy / Box type
+         */
+        int type;
+
+        public Block(long startingIndex, long count, int type) {
+            super();
+            this.startingIndex = startingIndex;
+            this.count = count;
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return "Block [startingIndex=" + startingIndex + ", count=" + count + ", type=" + type + "]";
+        }
+    }
+    
+    /**
+     * 
+     * @param indexType
+     * @param maxIndex
+     * @param currentIndex
+     * @return pair.  count of ==> type
+     */
+    static Block getBlock(TreeMap<Long,Integer> indexType, long maxIndex, long currentIndex ) {
+        //Get next highest value
+        Map.Entry<Long,Integer> nextEntry = indexType.ceilingEntry(currentIndex+1);
+        Map.Entry<Long,Integer> currentEntry = indexType.floorEntry(currentIndex);
+        
+        // 1 x 10 ; 2 x 20 ; 3 x 30 
+        //0 -> 1  ; 10 -> 2 ; 30 -> 3
+        if (nextEntry == null) {
+            return new Block(currentIndex, maxIndex - currentIndex, currentEntry.getValue());            
+        } else {
+            return new Block(currentIndex, nextEntry.getKey() - currentIndex, currentEntry.getValue());            
+        }
+    }
+    
+    private static long getBlockStartCount(TreeMap<Long,Long> prevRow, Block blockOfB) {
+        long prevCountLastRow = getMaxBeforeIndex(prevRow, blockOfB.startingIndex);
+        
+        return prevCountLastRow;
+    }
+    
+    private static void updateRow(TreeMap<Long,Long> row, long blockStartIndex, long blockEndIndex, long blockStartCount, long nextBlockRowIndex, int[][] bruteForce) {
+        long blockLength = blockEndIndex - blockStartIndex + 1;
+        long blockEndCount = blockStartCount + blockLength - 1;
+                
+        row.put(blockStartIndex, blockStartCount);
+        row.put(blockEndIndex, blockEndCount);
+        
+        long checkRowIndex = nextBlockRowIndex - 1;
+        Preconditions.checkState(bruteForce[(int)checkRowIndex][(int) blockStartIndex] == (int)blockStartCount);            
+        Preconditions.checkState(bruteForce[(int)checkRowIndex][(int) blockEndIndex] == (int)blockEndCount);
+        
+    }
+    
+    /**
+     * 
+     * @return the maximum start, how much length remaining
+     */
+    private static long[] getMaximumBlockStart(TreeMap<Long,Long> prevRow,
+            long maxLength, long currentIndex, List<Block> prevMatchingBlocks) {
+        
+        //Check maxLength through 1 and all defined points 
+        
+        //Intialize with -1
+        long[] maxStart = new long[] { 1 + getMaxBeforeIndex(prevRow, currentIndex), 1 };
+        
+        long prevBlockCombinedLen = 1;
+        for(int prevBlockIdx = prevMatchingBlocks.size() - 1; prevBlockIdx >= 0; --prevBlockIdx) {
+            Block prevBlock = prevMatchingBlocks.get(prevBlockIdx);
+            
+            prevBlockCombinedLen += prevBlock.count;
+            
+            long lenNeededStartBlock = prevBlockCombinedLen;            
+            long lenNeededEndBlock = lenNeededStartBlock - prevBlock.count;
+            
+            long searchIndexStart = prevBlock.startingIndex - 1;
+            long searchIndexEnd = searchIndexStart + prevBlock.count;
+            
+            if (lenNeededEndBlock > maxLength)
+                break;
+            
+            if (lenNeededStartBlock > maxLength) {
+                searchIndexStart += lenNeededStartBlock - maxLength;
+            }
+            
+            //Check directly the beginning and end of the block
+            long count = getMaxBeforeIndex(prevRow, searchIndexStart+1) + lenNeededStartBlock;
+            
+            if (count > maxStart[0]) {
+                maxStart = new long[] { count, lenNeededStartBlock };
+            }
+            
+            count = getMaxBeforeIndex(prevRow, searchIndexEnd+1) + lenNeededEndBlock;
+            
+            if (count > maxStart[0]) {
+                maxStart = new long[] { count, lenNeededEndBlock };
+            }
+            
+            NavigableSet<Long> keys = prevRow.subMap(searchIndexStart, true, searchIndexEnd, true).descendingKeySet();
+        
+            for(Long index : keys) {
+                long lengthUsed = lenNeededStartBlock - (index - searchIndexStart);
+                count = prevRow.get(index) + lengthUsed;
+                
+                if (count > maxStart[0]) {
+                    log.debug("!!! Found a higher start");
+                    maxStart = new long[] { count, lengthUsed };
+                }
+            }
+        }
+        
                 
         
+       return maxStart; 
+    }
+    
+    static List<Block> getMatchingBlocksOfB(long bMaxIndex, TreeMap<Long,Integer> bIndexType, Block blockOfA) {
+        
+        List<Block> blocksOfB = Lists.newArrayList();
+        
+        long currentColIndex = 0;
+        while(currentColIndex < bMaxIndex ) {
+            Block blockOfB = getBlock(bIndexType, bMaxIndex, currentColIndex);
+           // log.debug("bPair count {} type {}  currentColIndex {}", bPair.getLeft(), bPair.getRight(), currentColIndex);
+            
+            if (blockOfB.type == blockOfA.type) {
+                blocksOfB.add(blockOfB);
+            }
+            
+            currentColIndex += blockOfB.count;
+        }
+        
+        return blocksOfB;
+    }
+    
+    /**
+     * 
+     * @param row index --> count
+     * @param currentRowIndex
+     * @return
+     */
+    static long getMaxBeforeIndex(TreeMap<Long,Long> row, long currentRowIndex) {
+        Map.Entry<Long,Long> indexCount  = row.floorEntry(currentRowIndex - 1);
+        Map.Entry<Long,Long> nextIndexCount = row.ceilingEntry(currentRowIndex);
+        
+        //If the next value is present, use it
+        //say we want 5 and the row has 3 -> 5  10 - > 12, return value 7
+        long max  = 0;
+        
+        if (nextIndexCount != null ) {
+            long diffIndexes = nextIndexCount.getKey() - currentRowIndex;
+            if (nextIndexCount.getValue() >= diffIndexes) { 
+                // minus one because we want the value before current index
+                max = nextIndexCount.getValue() - diffIndexes - 1;
+            }
+        }
+        
+        if (indexCount != null) {
+            //Cannot be less than previous value either
+            max = Math.max(max, indexCount.getValue());
+        }
+        
+        return max;
+    }
+    
+    private long[] buildIndexTypeArrays(TreeMap<Long,Integer> aIndexType, TreeMap<Long,Integer> bIndexType, InputData in) {
+long currentColIndex = 0;
+        
+        for(int n = 0; n < in.N; ++n) {
+            Pair<Long,Integer> pair = in.a.get(n);
+            aIndexType.put(currentColIndex, pair.getValue());
+                
+            currentColIndex += pair.getLeft();
+        }
+        
+        long aMaxIndex = currentColIndex;
+        
+        
+        currentColIndex = 0;
+        for(int m = 0; m < in.M; ++m) {
+            Pair<Long,Integer> pair = in.b.get(m);
+            bIndexType.put(currentColIndex, pair.getValue());
+                
+            currentColIndex += pair.getLeft();
+        }
+        long bMaxIndex = currentColIndex;
+        
+        return new long[] {aMaxIndex, bMaxIndex};
+    }
+    
+    private TreeMap<Long,Long> buildFirstBlockRow(InputData in, TreeMap<Long,Integer> bIndexType, long bMaxIndex, int[][] bruteForce) {
+      //Initialize first row
+        TreeMap<Long,Long> blockRow = new TreeMap<>();
+        Pair<Long,Integer> aPair = in.a.get(0); //getPair(aIndexType, aMaxIndex, 0);
+        long currentColIndex = 0;
+        long aPairRemaining = aPair.getKey();
+        
+        long currentRowIndex = aPair.getKey() - 1;
+        
+        
+        while(currentColIndex < bMaxIndex && aPairRemaining > 0) {
+            Block bPair = getBlock(bIndexType, bMaxIndex, currentColIndex);
+            
+            if (bPair.type != aPair.getValue()) {
+                currentColIndex += bPair.count;
+                continue;
+            }
+            
+            long overlap = Math.min(aPairRemaining, bPair.count);
+            
+            long prevCount = getMaxBeforeIndex(blockRow, currentColIndex);
+            
+            long blockStartCount = prevCount + 1;
+            long blockEndCount = prevCount + overlap;
+            long blockStartIndex = currentColIndex;
+            long blockEndIndex = currentColIndex + overlap - 1;
+            blockRow.put(blockStartIndex, blockStartCount);
+            blockRow.put(blockEndIndex, blockEndCount);
+            Preconditions.checkState(bruteForce[(int)currentRowIndex][(int)currentColIndex] == (int)prevCount + 1);            
+            Preconditions.checkState(bruteForce[(int)currentRowIndex][(int) blockEndIndex] == (int)blockEndCount);
+                        
+            aPairRemaining -= overlap;
+            currentColIndex += overlap;
+        }
+        
+        return blockRow;
+    }
+    
+    
+    public String handleCase(InputData in) {
+
+        int[][] bruteForce = bruteForce(in);
+
+        // Count --> type
+        TreeMap<Long, Integer> aIndexType = new TreeMap<>();
+        TreeMap<Long, Integer> bIndexType = new TreeMap<>();
+
+        long[] maxes = buildIndexTypeArrays(aIndexType, bIndexType, in);
+
+        long aMaxIndex = maxes[0];
+        long bMaxIndex = maxes[1];
+
+        // Index --> Count
+        TreeMap<Long, Long> blockRow = buildFirstBlockRow(in, bIndexType, bMaxIndex, bruteForce);
+
+        // Index in bruteForceArray corresponding to when blockOfA has been fully processed
+        long endBlockOfARowIndex = in.a.get(0).getLeft() - 1;
+
+        log.debug("After first row {}", blockRow);
+
+        TreeMap<Long, Long> prevBlockRow = blockRow;
+        blockRow = new TreeMap<>();
+
+        // Go through each A block
+        for (int i = 1; i < in.N; ++i) {
+
+            Block blockOfA = getBlock(aIndexType, aMaxIndex, endBlockOfARowIndex+1);
+            endBlockOfARowIndex += blockOfA.count;
+
+            //TreeMap<Long, Long> prevRow = new TreeMap<Long, Long>();
+
+            log.debug("Processing block A {} ", blockOfA);
+
+            List<Block> matchingBlocks = getMatchingBlocksOfB(bMaxIndex, bIndexType, blockOfA);
+
+
+            for (int mbIndex = 0; mbIndex < matchingBlocks.size(); ++mbIndex) {
+
+                Block blockOfB = matchingBlocks.get(mbIndex);
+
+                // Long nextBlockStartCount = mbIndex == matchingBlocks.size() - 1 ? null :
+                // getBlockStartCount(prevRow, matchingBlocks.get(mbIndex + 1));
+
+                long[] startCount = getMaximumBlockStart(prevBlockRow, blockOfA.count, blockOfB.startingIndex, matchingBlocks.subList(0, mbIndex));
+
+                long aBlockLengthRemaining = blockOfA.count - startCount[1];
+                long bBlockLengthRemaining = blockOfB.count - 1;
+
+                long overlap = Math.min(aBlockLengthRemaining, bBlockLengthRemaining);
+
+                updateRow(blockRow, blockOfB.startingIndex, blockOfB.startingIndex + overlap, 
+                        startCount[0], endBlockOfARowIndex, bruteForce);
+
+
+                // also match at the end
+                if (bBlockLengthRemaining <= aBlockLengthRemaining) {
+                    // blockA fits exactly in blockB
+                    continue;
+                }
+
+                // Block A is smaller
+                long offset = blockOfB.startingIndex + bBlockLengthRemaining - aBlockLengthRemaining;
+
+                startCount = getMaximumBlockStart(prevBlockRow, blockOfA.count, blockOfB.startingIndex + offset, matchingBlocks.subList(0, mbIndex));
+
+                updateRow(blockRow, blockOfB.startingIndex, blockOfB.startingIndex + overlap, startCount[0], endBlockOfARowIndex, bruteForce);
+            }
+
+            log.info("Row {} ", blockRow);
+            // Index --> count
+
+            // Remove all previousRow entries that have been improved
+            Iterator<Map.Entry<Long, Long>> prevRowIt = prevBlockRow.entrySet().iterator();
+            while (prevRowIt.hasNext()) {
+
+                Map.Entry<Long, Long> entry = prevRowIt.next();
+                long count = getMaxBeforeIndex(blockRow, entry.getKey() + 1);
+                if (count >= entry.getValue()) {
+                    prevRowIt.remove();
+                }
+            }
+
+            // Now add all row entries
+            for (Map.Entry<Long, Long> entry : blockRow.entrySet()) {
+                Preconditions.checkState(!prevBlockRow.containsKey(entry.getKey()) || prevBlockRow.get(entry.getKey()).equals(entry.getValue()));
+
+                prevBlockRow.put(entry.getKey(), entry.getValue());
+            }
+            log.info("New prev row {}", prevBlockRow);
+            blockRow = new TreeMap<>();
+        }
+
+        long max = prevBlockRow.get(prevBlockRow.lastKey());
+
+        long maxBF = bruteForce[bruteForce.length - 1][bruteForce[0].length - 1];
+
+        Preconditions.checkState(max == maxBF);
         return String.format("Case #%d: No", in.testCase);
     }
 
