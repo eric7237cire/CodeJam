008da4912de3d66fa1faa258d58758152873cd7a
unknown <G-CG34-FRMP@B016653.fr.ad.bull.net>
2013-01-18 09:57:54 +0100

    letter

--- cjava/src/main/java/codejam/y2010/round_final/letter_stamper/Main.java
+++ cjava/src/main/java/codejam/y2010/round_final/letter_stamper/Main.java
@@ -23,8 +23,8 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
     @Override
     public String[] getDefaultInputFiles() {
        //return new String[] {"sample.in"};
-//        return new String[] {"D-small-practice.in"};
-        return new String[] {"A-small-practice.in", "A-large-practice.in"};
+        return new String[] {"A-small-practice.in"};
+       // return new String[] {"A-small-practice.in", "A-large-practice.in"};
     }
     
 
@@ -73,6 +73,12 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         return -1;
     }
     
+    int[][] charsToStack = new int [][] {
+            { -1, 0, 1}, //A B ; A C
+            { 2, -1, 3}, //B A B C
+            {4, 5, -1}, // C A  C B
+    };
+    
     /*
      * 0 ABC ABC ==> C AB CAB (4)
      * 1 ACB ACB ==> B AC BAC (2)
@@ -97,6 +103,7 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
      */
     int[] popTransition = new int[] { 3, 5, 1, 4, 0, 2};
     int[][] topLetterToStack       = new int[][] { {0, 1}, {2, 3}, {4, 5} };
+    int[] stackToTopLetter = new int[] { 0, 0, 1, 1, 2, 2 };
     
     //Stack type to letter
     int[][] popsNeeded = new int[][] { 
@@ -180,6 +187,99 @@ public class Main implements TestCaseHandler<InputData>, TestCaseInputScanner<In
         return memo[currentIndex][stackType][stackHeight] = Math.min(costPop, costPush);
         
         
+    }
+    
+    public String handleCaseBottomUp(InputData in) {
+        
+        //index -- state -- stack height
+        int[][][] bottUp = new int[in.S.length()][6][in.S.length()];
+        for(int i = 0; i < in.S.length(); ++i)
+            for(int j = 0; j < in.S.length(); ++j)
+                for(int k = 0; k < 6; ++k)
+                    bottUp[i][k][j] = -1;
+        
+        int curIdx = in.S.length() - 1;
+        int curChar = (int) in.S.charAt(curIdx) - 'A';
+        
+        //Initialize last character
+        
+        //Stack height = 0
+        for(int stackState = 0; stackState < 6; ++stackState) {
+            //No matter what the stack state, it will cost 3.  push,print,pop
+            bottUp[curIdx][stackState][0] = 3;
+        }
+        
+        //Stack height 1
+        for(int stackState = 0; stackState < 6; ++stackState) {
+            if (popsNeeded[stackState][curChar] == 0)
+                //print, pop
+                bottUp[curIdx][stackState][1] = 2; 
+            else 
+                //We need to push a 2nd character, and pop it x 2, print
+                bottUp[curIdx][stackState][1] = 4;
+        }
+        
+        //Stack height 2
+        for(int stackState = 0; stackState < 6; ++stackState) {
+            if (popsNeeded[stackState][curChar] < 2)
+                //print, pop, pop
+                bottUp[curIdx][stackState][2] = 3; 
+            else 
+                //pop, pop, push, print, pop
+                bottUp[curIdx][stackState][2] = 5;
+        }
+        
+        for(int stackHeight = 3; stackHeight < in.S.length(); ++stackHeight) {
+            //Pop x stackheiht + print
+            for(int stackState = 0; stackState < 6; ++stackState) { 
+                bottUp[curIdx][stackState][stackHeight] = 1 + stackHeight;
+            }
+        }
+        
+        //General case        
+        for( curIdx = in.S.length() - 2; curIdx >= 0; --curIdx) {
+        
+            curChar = (int) in.S.charAt(curIdx) - 'A';
+            
+            //Height 0
+            for(int stackState = 0; stackState < 6; ++stackState) {
+                
+                int stackState1 = topLetterToStack[curChar][0];
+                int stackState2 = topLetterToStack[curChar][1];
+                
+                //Sanity check, the 2 states are equivalent when stack height = 1
+                Preconditions.checkState(bottUp[curIdx+1][stackState1][1] 
+                        == bottUp[curIdx+1][stackState2][1]);
+                
+                //Note the stackState is ignored, only the curChar matters
+                bottUp[curIdx][stackState][0] = 2 + bottUp[curIdx+1][stackState1][1];
+            }
+            
+            //Height 1
+            for(int stackState = 0; stackState < 6; ++stackState) {
+                if (popsNeeded[stackState][curChar] == 0)
+                    //print
+                    bottUp[curIdx][stackState][1] = 1 + bottUp[curIdx+1][stackState][1]; 
+                else {
+                    //We need to push a 2nd character
+                    int stackTopChar = stackToTopLetter[stackState];
+
+                    
+                    int nextStackState = charsToStack[curChar][stackTopChar];
+                    Preconditions.checkState(nextStackState >= 0);
+                    
+                    int pushCost = 2 + bottUp[curIdx+1][nextStackState][2];
+                    
+                    //Or we can always pop
+                    int popCost = bottUp[curIdx][stackState][0] + 1;
+                    
+                    bottUp[curIdx][stackState][1] = Math.min(pushCost, popCost);
+                }
+            }
+        }
+        
+        
+        return "a";
     }
 
     @Override
