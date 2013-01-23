65c107482047b7df948a2266fe22a9d9f0340d95
Eric Gron <eric7237cire@gmail.com>
2013-01-23 22:49:02 +0100

    a

--- 2011/qual-africa/compile.bat
+++ 2011/qual-africa/compile.bat
@@ -8,10 +8,17 @@ rem close-loop.exe sample.in
 rem close-loop.exe A-small-practice.in 
 rem close-loop.exe A-large-practice.in 
 
-g++ -std=c++11 investing.cpp -o investing.exe
+rem g++ -std=c++11 investing.cpp -o investing.exe
 
-investing.exe sample.in
+rem investing.exe sample.in
 
-investing.exe B-small-practice.in 
-investing.exe B-large-practice.in 
+rem investing.exe B-small-practice.in 
+rem investing.exe B-large-practice.in 
+
+ g++ -std=c++11 build-house.cpp -o build-house.exe
+
+ build-house.exe sample.in
+
+rem build-house.exe B-small-practice.in 
+rem build-house.exe B-large-practice.in 
 
