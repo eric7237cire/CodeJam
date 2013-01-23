261d63e6524f6c62b900903def74ed6a797c0186
Eric Gron <eric7237cire@gmail.com>
2013-01-23 09:12:47 +0100

    rh

--- 2011/qual-africa/compile.bat
+++ 2011/qual-africa/compile.bat
@@ -1,23 +1,17 @@
 cls
 @echo off
-rem g++ -std=c++11 oddman-out.cpp -o oddman-out.exe 
 
-rem oddman-out.exe A-small-practice.in > a_small.txt 
+rem g++ -std=c++11 close-loop.cpp -o close-loop.exe
 
-rem oddman-out.exe A-large-practice.in > a_large.txt
+rem close-loop.exe sample.in
 
+rem close-loop.exe A-small-practice.in 
+rem close-loop.exe A-large-practice.in 
 
- rem g++ -std=c++11 get-to-work.cpp -o get-to-work.exe 
- rem 2>&1 | less
-rem get-to-work.exe sample.in
- rem get-to-work.exe B-small-practice.in > b_small.txt
- rem get-to-work.exe B-large-practice.in > b_large.txt
+g++ -std=c++11 investing.cpp -o investing.exe
 
-g++ -std=c++11 close-loop.cpp -o close-loop.exe
+investing.exe sample.in
 
-close-loop.exe sample.in 
-
-close-loop.exe A-small-practice.in 
-
-close-loop.exe A-large-practice.in 
+investing.exe B-small-practice.in 
+investing.exe B-large-practice.in 
 
---
+++ 2011/qual-africa/investing.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <sstream>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <functional>
+#include <numeric>
+#include <array>
+#include <queue>
+#include "prettyprint.h"
+using namespace std;
+
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef long long ll;
+
+#define FORE(k,a,b) for(uint k=(a); k <= (b); ++k)
+#define FOR(k,a,b) for(uint k=(a); k < (b); ++k)
+
+#define pb push_back 
+#define mp make_pair 
+
+typedef vector<int> vi; 
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<uint> uvi; 
+typedef vector<uvi> uvvi;
+typedef vector<vd> vvd;
+typedef pair<int,int> ii;
+typedef pair<uint,uint> uu;
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end() 
+#define FOR_IT(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++) 
+#define contains(c,x) ((c).find(x) != (c).end()) 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+
+void do_test_case(int test_case, ifstream& input, ofstream& output);
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+int main(int argc, char** args)
+{
+	/*
+  if (argc < 2) {
+    cerr << "Usage: <exe> input_file" << endl;
+    return -1;
+  }
+  */
+  ifstream input;
+  string inputFilename = (argc < 2) ? "sample.in" : args[1];
+      
+   
+  input.open(inputFilename);
+
+  string outputFilename = inputFilename;
+  replace(outputFilename, ".in", ".out");
+  
+  ofstream output;
+  output.open(outputFilename);
+  
+  int T;
+  input >> T;
+  
+  //string line;
+  //getline(input, line);
+    
+  for (int test_case = 0; test_case < T; ++test_case) 
+  {
+    do_test_case(test_case, input, output);  
+  }
+
+}
+
+
+template <class K, class V> 
+V getMapValue( const map<K,V>& aMap, const K& key, const V& defaultValue )
+{
+    typename map<K, V>::const_iterator it = aMap.find(key);
+    if ( it == aMap.end() )
+        return defaultValue;
+    return it->second;
+}
+
+void do_test_case(int test_case, ifstream& in, ofstream& fout)
+{
+    uint M;
+       
+    
+    in >> M;
+    
+    uvi P(12);
+    
+    FOR(i, 0, 12)
+        in >> P[i];
+    
+    uint bestStartIdx = 0;
+    uint bestStopIdx;
+    uint bestGain = 0;
+    
+    FOR(start, 0, 12)
+    {
+        auto it = max_element( P.begin() + start, P.end());
+        uint units = M / P[start];
+        uint leftOver = M - units * P[start];
+        uint gain = units * (*it) - units * P[start];
+        if (gain > bestGain || ( gain == bestGain && P[start] < P[bestStartIdx]) )
+        {
+            bestGain = gain;
+            bestStartIdx = start + 1;
+            bestStopIdx = distance(P.begin(), it) + 1;
+        }
+    }
+    
+    if (bestGain == 0)
+    {
+        fout << "Case #" << test_case+1 << ": IMPOSSIBLE"
+     << endl;
+    } else {
+        fout << "Case #" << test_case+1 << ": " << bestStartIdx
+        << " " << bestStopIdx << " " << bestGain << endl;
+    }
+       
+		
+
+}
+
