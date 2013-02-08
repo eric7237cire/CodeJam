e6af0746066c67ee8cebaacdccc2b79791a6e375
unknown <G-CG34-FRMP@B016653.fr.ad.bull.net>
2013-02-08 09:54:25 +0100

    a

---
+++ cjava/src/main/java/codejam/y2008/round_pracContest/square_fields/InputData.java
@@ -0,0 +1,18 @@
+package codejam.y2008.round_pracContest.square_fields;
+
+import codejam.utils.main.AbstractInputData;
+
+public class InputData extends AbstractInputData {
+
+    
+    long F;
+    long D; //how many eggs can we drop
+    long B; //how many eggs can be broken
+    
+    
+    
+    public InputData(int testCase) {
+        super(testCase);
+    }
+
+}
