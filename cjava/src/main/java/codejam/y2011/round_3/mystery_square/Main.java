ab5820975f4dbcaf6e79b51f43c72d3c75c8e24c
unknown <G-CG34-FRMP@B016653.fr.ad.bull.net>
2013-02-13 16:09:03 +0100

    a

--- cjava/src/main/java/codejam/y2011/round_3/mystery_square/Main.java
+++ cjava/src/main/java/codejam/y2011/round_3/mystery_square/Main.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.common.math.LongMath;
 
 import codejam.utils.main.InputFilesHandler;
@@ -45,26 +46,110 @@ public class Main extends InputFilesHandler implements TestCaseHandler<InputData
             int len = binaryStr.length();
             
             int mysStartIndex = len / 2;
-            //int halfIndex = (len+1) / 2 - 1;
+            
+            //Half using ? marks
             String mystery = binaryStr.substring(0, mysStartIndex) +
                     StringUtils.repeat('?', binaryStr.length() - mysStartIndex);
             
             log.debug("Mystery {}", mystery);
             
+            //Calculate square root rounded down
             String withOnesStr = mystery.replace('?', '1');
             long withOnes = Long.parseLong(withOnesStr, 2);
             long sqrt = LongMath.sqrt(withOnes, RoundingMode.DOWN);
             log.debug("Replace with ones {} ({}) take sqrt rounded down = {} ", 
                     withOnesStr, withOnes, sqrt );
+            
+            
+            
         }
         
+        
+    }
+    
+    public void testBottomUp() {
+        for(long i = 2; i < 50; ++i) {
+            long ii = i * i;
+            log.debug("\nSquare root of {} ({}): {} ({})", 
+                    Long.toBinaryString(ii), ii, Long.toBinaryString(i), i);
+            
+            
+            String binaryStr = Long.toBinaryString(ii);
+            
+            int len = binaryStr.length();
+            
+            int mysStopIndex = (len+1) / 2;
+            
+            String mystery = StringUtils.repeat('?', mysStopIndex) 
+                    + binaryStr.substring(mysStopIndex, binaryStr.length()) ;
+            
+            log.debug("Mystery {}", mystery);
+            
+        }
+    }
+    
+    //Guessing last 2 digits are 11
+    public void findSquareRootBottomUp(long lowerSquareDigits, int numLowerSquareDigits,
+            long initialSquareRootGuess)
+    {
+        Preconditions.checkState(initialSquareRootGuess == 3 || initialSquareRootGuess == 1);
+        /**
+         * say we know the square root ends in
+         * ----11
+         * then the square root must be 4A + 3
+         * 
+         * ==16A^2 + 3*4*2A + 9 = 24A + 9
+         * 
+         * then take lowerSquareDigits % 16
+         */
+        long currentSquareRootGuess = initialSquareRootGuess;
+        
+        log.debug("lower square {} ({})", Long.toBinaryString(lowerSquareDigits), lowerSquareDigits);
+        
+        
+        
+        for(int sqRootDigit = 3; sqRootDigit < numLowerSquareDigits; ++sqRootDigit)
+        {
+            int currentSquareRootMod = 1 << sqRootDigit - 1;
+            
+            log.debug("Square root = {}*A + {}", currentSquareRootMod, currentSquareRootGuess);
+            long coefA = currentSquareRootMod * 2 * currentSquareRootGuess;
+            long constant = currentSquareRootGuess*currentSquareRootGuess;
+            
+            int currentSquareMod = currentSquareRootMod << 2;
+            
+            log.debug("(Square root)^2 = {}*A^2 + {}*A + {}", 
+                    currentSquareMod, coefA, constant);
+            
+            // currentSquareRootGuessMod * A^2 + coefA * A + constant = square
+            
+            
+            long mustEqual = lowerSquareDigits % currentSquareMod;
+            
+            log.debug("(Square root)^2 % {} = {}", 
+                    currentSquareMod, mustEqual);
+            
+            long aIsOne = (coefA * 1 + constant) % currentSquareMod;
+            long aIsZero = (coefA * 0 + constant) % currentSquareMod;
+            
+            if (aIsOne == mustEqual) {
+                currentSquareRootGuess |= (1 << sqRootDigit - 1);
+            } else {
+                //it's a zero
+                Preconditions.checkState(aIsZero == mustEqual);
+            }
+            
+            log.debug("Current square root guess {} ({})", Long.toBinaryString(currentSquareRootGuess), currentSquareRootGuess);
+        }
         
     }
   
     @Override
     public String handleCase(InputData in) {
        
-        testTopDown();
+       // testTopDown();
+        //testBottomUp();
+        findSquareRootBottomUp(361 % (1 << 6), 6,  3);
 
         return String.format("Case #%d: ", in.testCase);
     }
