e37ee968f09f27c4cc80b41e2dd45cca51df0ba4
unknown <G-CG34-FRMP@B016653.fr.ad.bull.net>
2013-02-26 11:05:45 +0100

    a

---
+++ uva/270 - Lining Up/test.bat
@@ -0,0 +1,11 @@
+rem python gen_data.py > in.txt
+
+g++ -DONLINE_JUDGE -O2 lining-up.cpp -o p0.exe
+
+rem g++ -DONLINE_JUDGE -O2 p1.cpp -o p1.exe
+
+cat input.txt | p0 > o.txt
+rem cat in.txt | p1 > o2.txt
+
+
+rem diff o.txt o2.txt
