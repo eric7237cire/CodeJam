d0f2f49e2d418cab893f5b14084509903a018dc6
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-02-28 16:08:05 +0100

    a

--- USACO/milk4.cpp
+++ USACO/milk4.cpp
@@ -111,7 +111,7 @@ main ()
     fscanf (fin, "%d", &(pails[i]));  
     fclose (fin);  
   
-    qsort (pails, nPail, sizeof (pails[0]), compare);  
+    sort (pails, pails + nPail, greater<int>());  
   
     findMinPails ();  
     backtrack ();  
@@ -119,10 +119,10 @@ main ()
     FILE   *fout = fopen ("milk4.out", "w");  
     fprintf (fout, "%d", nFinal);  
   
-    for (int i = 0; i < nFinal; i++)  
-    fprintf (fout, " %d", finalSet[i]);  
-    fprintf (fout, "/n");  
-    fclose (fin);  
+    FOR(i, 0, nFinal)  
+    	    fprintf (fout, " %d", finalSet[i]);  
+    fprintf (fout, "\n");  
+    
   
     return 0;  
 }  
@@ -131,56 +131,59 @@ void
 findMinPails ()  
 {  
     //Indicate that nothing is possible yet  
-    for (int i = 0; i <= nQuart; i++) {  
-    lastPail[i] = -1;  
-    minPails[i] = INF;  
-    nLast[i] = -1;  
+    FORE(i, 0, nQuart)
+    {  
+    	    lastPail[i] = -1;  
+    	    minPails[i] = INF;  
+    	    nLast[i] = -1;  
     }  
   
     minPails[0] = 0;  
   
-    for (int i = 0; i < nPail; i++) {  
-    int     tempMP[MAXQ], tempLP[MAXQ], tempNL[MAXQ];  
-  
-    for (int j = 0; j <= nQuart; j++) {  
-        tempMP[j] = minPails[j];  
-        tempLP[j] = lastPail[j];  
-        tempNL[j] = nLast[j];  
-    }  
-  
-    for (int j = pails[i]; j <= nQuart; j++) {  
-        int     prev = j - pails[i];  
-  
-        if (tempMP[prev] < INF) {    /* meaning it's reachable */  
-//case 1: try using more of this pail  
-            if (tempLP[prev] == pails[i]) {  
-            tempLP[j] = pails[i];  
-            tempMP[j] = tempMP[prev];  
-            tempNL[j] = tempNL[prev] + 1;  
-        }  
-  
-//case 2: try using just one of this pail  
-            // because we have to  
-            if (tempLP[prev] != pails[i]) {  
-            tempLP[j] = pails[i];  
-            tempMP[j] = tempMP[prev] + 1;  
-            tempNL[j] = 1;  
-        }  
-  
-//case 3: try using just one of this pail  
-        /* because it's better */  
-            if (minPails[prev] < INF &&  
-            (minPails[prev] + 1 < tempMP[j] ||  
-            (minPails[prev] + 1 == tempMP[j] &&  
-                betterSequence (prev,  
-                j - tempNL[j] * pails[i])  
-            ))) {  
-            tempLP[j] = pails[i];  
-            tempMP[j] = minPails[prev] + 1;  
-            tempNL[j] = 1;  
-        }  
-        }  
-    }  
+    for (int i = 0; i < nPail; i++) 
+    {  
+	    int     tempMP[MAXQ], tempLP[MAXQ], tempNL[MAXQ];  
+	  
+	    for (int j = 0; j <= nQuart; j++) {  
+		tempMP[j] = minPails[j];  
+		tempLP[j] = lastPail[j];  
+		tempNL[j] = nLast[j];  
+	    }  
+	  
+	    for (int j = pails[i]; j <= nQuart; j++) {  
+		int     prev = j - pails[i];  
+	  
+		if (tempMP[prev] < INF) {    /* meaning it's reachable */  
+		//case 1: try using more of this pail  
+			    if (tempLP[prev] == pails[i]) {  
+			    tempLP[j] = pails[i];  
+			    tempMP[j] = tempMP[prev];  
+			    tempNL[j] = tempNL[prev] + 1;  
+			}  
+		  
+		//case 2: try using just one of this pail  
+			    // because we have to  
+			    if (tempLP[prev] != pails[i]) {  
+			    tempLP[j] = pails[i];  
+			    tempMP[j] = tempMP[prev] + 1;  
+			    tempNL[j] = 1;  
+			}  
+		  
+		//case 3: try using just one of this pail  
+			/* because it's better */  
+			    if (minPails[prev] < INF &&  
+			    (minPails[prev] + 1 < tempMP[j] ||  
+			    (minPails[prev] + 1 == tempMP[j] &&  
+				betterSequence (prev,  
+				j - tempNL[j] * pails[i])  
+			    ))) {  
+			    tempLP[j] = pails[i];  
+			    tempMP[j] = minPails[prev] + 1;  
+			    tempNL[j] = 1;  
+			}  
+		}
+	    }
+      
   
     for (int j = pails[i]; j <= nQuart; j++)  
         if (tempMP[j] <= minPails[j]) {  
@@ -223,8 +226,3 @@ betterSequence (int a, int b)
     return 1;  
 }  
   
-int   
-compare (const void *a, const void *b) //Sort in descending order  
-{  
-    return *(int *) b - *(int *) a;  
-}  
