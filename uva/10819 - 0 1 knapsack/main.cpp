372d21fb8762734fb23dfb0db24ac0bc0bd6456c
Eric Gron <eric7237cire@gmail.com>
2013-03-10 22:33:54 +0100

    h

--- uva/10819 - 0 1 knapsack/main.cpp
+++ uva/10819 - 0 1 knapsack/main.cpp
@@ -1,126 +1,79 @@
 #include "stdio.h"
 #include <algorithm>
-//#include <vector>
-#include <cstring>
-//#include "string.h"
-#include <limits>
-
-//#include <string>
-
+//#include <cstring>
+//#include <limits>
 #include <stdlib.h>
-//#include <ctype.h>
-
-//#include <iostream>
 
 using namespace std;
 
 #define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
 #define pb push_back
 
-typedef unsigned int uint; 
+const int MAX_M = 10200;
+const int MAX_N = 100;
+int P[MAX_N];
+int V[MAX_N];
+int N;
+int M;
 
-char buf[1024];
+//memo[Index][money left][refund]
+int memo[MAX_N][1 + MAX_M][2];
 
-const int items = 30;
-const int maxSpace = 1000;
-int V[items];
-int W[items];
-int depth[items];
-char* next;
+const int INF = 100000;
 
-int maxIdx;
-
-int memo[items][1 + maxSpace];
-int used[items][1 + maxSpace];
-
-int value(int idx, int left, int& usedItems)
+int value(const int idx, const int left, const int refund)
 {
-	//Considered all the coins
-	if (idx == maxIdx || left == 0)
+	//penalize if we used refund illegally
+	if (idx == N && refund && M - left <= 1800)
+		return -INF;
+	
+	if (idx == N )
 		return 0;
 		
-	if (memo[idx][left] != -1)
+	int& res = memo[idx][left][refund];
+	
+	if (res != -1)
+		return res;
+	
+	int valIfSkip = value(1+idx, left, refund);
+	
+	if (left < P[idx]) 
 	{
-		usedItems |= used[idx][left];
-		return memo[idx][left];
-	}
-		
-		
-	//Can't use item
-	if (W[idx] > left)
-		return value(1+idx, left, usedItems);
-		
-	int usedIfSkip = usedItems;
-	int usedIfUse = usedItems;
-	int valIfSkip = value(1+idx, left, usedIfSkip);
-	int valIfUse =  V[idx] + value(1+idx, left - W[idx], usedIfUse );
-	
-	if (valIfUse >= valIfSkip) {
-		//printf("Used idx memo[%d] left %d  used %d \n", idx, left,  usedIfUse);
-		usedItems |= usedIfUse | 1 << idx; 
-	} else {
-		usedItems |= usedIfSkip;
+		return res = valIfSkip;
 	}
 	
-	memo[idx][left] = max(valIfSkip, valIfUse);
-	used[idx][left] = usedItems;
-	//printf("memo[%d] left %d = %d  used %d \n", idx, left, memo[idx][left], usedItems);
-	return memo[idx][left];
-}
-
-int NumberOfSetBits(int i)
-{
-    i = i - ((i >> 1) & 0x55555555);
-    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-    return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+	int valIfUse =  V[idx] + value(1+idx, left - P[idx], refund); 
+	
+	res = max(valIfSkip, valIfUse);
+	
+	//printf("memo[ idx %d][ left %d][refund %d] = %d  money used %d \n", idx, left, refund, memo[idx][left][refund], moneyUsed );
+	return res; 
 }
 
 int main()
 {
-#if !ONLINE_JUDGE && _MSC_VER && !__INTEL_COMPILER
-	freopen ("input.txt","r",stdin);
-#endif
-	int t, w;
-	bool isFirst = true;
-	while(2 == scanf("%d %d", &t, &w))
+	
+	
+	while(2 == scanf("%d%d", &M, &N))
 	{
-		memset(memo, -1, sizeof memo);
-		
-		if (isFirst) {
-			isFirst = false;
-		} else {
-			fputs("\n", stdout);
-		}
-		
-		scanf("%d", &maxIdx);
-		
-		FOR(i, 0, maxIdx)
+		FOR(i, 0, N)
 		{
-			int d, v;
-			scanf("%d %d", &d, &v);
-			W[i] = 3 * w * d;
-			depth[i] = d;
-			V[i] = v;
+			int p, f;
+			scanf("%d%d", &p, &f);
+			P[i] = p;
+			V[i] = f;
+			
+			FOR(m, 0, M+201)
+			{
+				memo[i][m][0] = -1;
+				memo[i][m][1] = -1;
+			}
 		}
 			
-		int usedItems = 0;
-		int ans = value(0, t, usedItems);
+		int ans = value(0, M, 0);
+		int ans2 = value(0, M+200, 1);
 		
-		usedItems = used[0][t];
-		
-		int usedTreasure = NumberOfSetBits(usedItems);
-		
-		//printf("max %d Ans %d\n", maxValue, ans);
-		printf("%d\n", ans );
-		printf("%d\n", usedTreasure );
-		
-		FOR(i, 0, maxIdx)
-		{
-			if (  (1 << i & usedItems) == 0)
-				continue;
-				
-			printf("%d %d\n", depth[i], V[i]);
-		}
+		printf("%d\n", max(ans,ans2) );
 	}
 
 	return 0;
