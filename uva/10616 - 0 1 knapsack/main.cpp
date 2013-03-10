abcb17449f955b75e12feb120bc9d193ce46cf5f
Eric Gron <eric7237cire@gmail.com>
2013-03-10 18:04:39 +0100

    h

--- uva/10616 - 0 1 knapsack/main.cpp
+++ uva/10616 - 0 1 knapsack/main.cpp
@@ -17,110 +17,79 @@ using namespace std;
 #define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
 #define pb push_back
 
-typedef unsigned int uint; 
+typedef long long ll;
 
-char buf[1024];
+//global maximums
+const int MAXN = 200;
+const int MAXQ = 10;
+const int MAXM = 10;
+const int MAXD = 20;
 
-const int items = 30;
-const int maxSpace = 1000;
-int V[items];
-int W[items];
-int depth[items];
-char* next;
+ll V[MAXN];
 
-int maxIdx;
+//problem maxs
+int N;
+int D;
 
-int memo[items][1 + maxSpace];
-int used[items][1 + maxSpace];
+/*
+	# of ways = memo[index up to][numbers remaining to use][cur remainder]
+*/
+ll memo[MAXN+1][MAXM][MAXD+1];
 
-int value(int idx, int left, int& usedItems)
+ll count(int idx, int nRem, int rem)
 {
-	//Considered all the coins
-	if (idx == maxIdx || left == 0)
-		return 0;
-		
-	if (memo[idx][left] != -1)
+	//base case, count 1 if current remainder is divisible by target 
+	if (nRem == 0)
 	{
-		usedItems |= used[idx][left];
-		return memo[idx][left];
-	}
-		
-		
-	//Can't use item
-	if (W[idx] > left)
-		return value(1+idx, left, usedItems);
-		
-	int usedIfSkip = usedItems;
-	int usedIfUse = usedItems;
-	int valIfSkip = value(1+idx, left, usedIfSkip);
-	int valIfUse =  V[idx] + value(1+idx, left - W[idx], usedIfUse );
-	
-	if (valIfUse >= valIfSkip) {
-		//printf("Used idx memo[%d] left %d  used %d \n", idx, left,  usedIfUse);
-		usedItems |= usedIfUse | 1 << idx; 
-	} else {
-		usedItems |= usedIfSkip;
+		return memo[idx][nRem][rem] = (rem + MAXD) % D == 0 ? 1 : 0;
 	}
 	
-	memo[idx][left] = max(valIfSkip, valIfUse);
-	used[idx][left] = usedItems;
+	if (idx == N)
+		return memo[idx][nRem][rem] = 0;
+		
+	if (memo[idx][nRem][rem] != -1)
+		return memo[idx][nRem][rem];
+	
+		
+	ll countIfSkip = ::count(1+idx, nRem, rem);
+	
+	int newRem = (V[idx] + rem) % MAXD;
+	
+	ll countIfUse =  ::count(1+idx, nRem - 1, newRem);
+		
+	return memo[idx][nRem][rem] = countIfSkip + countIfUse;
 	//printf("memo[%d] left %d = %d  used %d \n", idx, left, memo[idx][left], usedItems);
-	return memo[idx][left];
+	
 }
 
-int NumberOfSetBits(int i)
-{
-    i = i - ((i >> 1) & 0x55555555);
-    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-    return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
-}
 
 int main()
 {
 #if !ONLINE_JUDGE && _MSC_VER && !__INTEL_COMPILER
 	freopen ("input.txt","r",stdin);
 #endif
-	int t, w;
-	bool isFirst = true;
-	while(2 == scanf("%d %d", &t, &w))
+	int Q;
+	int t = 0;
+	
+	int M;
+	while(2 == scanf("%d %d", &N, &Q) && (N || Q))
 	{
-		memset(memo, -1, sizeof memo);
-		
-		if (isFirst) {
-			isFirst = false;
-		} else {
-			fputs("\n", stdout);
-		}
-		
-		scanf("%d", &maxIdx);
-		
-		FOR(i, 0, maxIdx)
+		FOR(i, 0, N)
 		{
-			int d, v;
-			scanf("%d %d", &d, &v);
-			W[i] = 3 * w * d;
-			depth[i] = d;
+			int v;
+			scanf("%d", &v);
 			V[i] = v;
 		}
 			
-		int usedItems = 0;
-		int ans = value(0, t, usedItems);
-		
-		usedItems = used[0][t];
-		
-		int usedTreasure = NumberOfSetBits(usedItems);
-		
-		//printf("max %d Ans %d\n", maxValue, ans);
-		printf("%d\n", ans );
-		printf("%d\n", usedTreasure );
-		
-		FOR(i, 0, maxIdx)
+		printf("SET %d:\n", ++t);
+		FOR(q, 0, Q)
 		{
-			if (  (1 << i & usedItems) == 0)
-				continue;
-				
-			printf("%d %d\n", depth[i], V[i]);
+			scanf("%d %d", &D, &M);
+			memset(memo, -1, sizeof memo);
+			
+			printf("QUERY %d: %lld\n", q+1, ::count(0, M, 0) );
 		}
+		
 	}
 
 	return 0;
