d24d2b3c1b12e85d20d8d57a8a562262bbd23d88
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-03-11 16:03:24 +0100

    a

--- uva/299 - adhoc easy/main.cpp
+++ uva/299 - adhoc easy/main.cpp
@@ -1,10 +1,11 @@
 #include "stdio.h"
-//#include <algorithm>
+#include <algorithm>
 //#include <vector>
 #include <cstring>
 //#include "string.h"
 //#include <limits>
 //#include <string>
+#include <list>
 #include <cassert>
 //#include <iostream>
 #include <stdlib.h>
@@ -21,18 +22,33 @@ int main()
 	
 	while(T--)
 	{
-		int f;
-		scanf("%d", &f);
+		int N;
+		scanf("%d", &N);
 		
-		int ans = 0;
-		while(f--)
+		list<int> trains;
+		
+		while(N--)
 		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
+			int train;
+			scanf("%d", &train);
+			
+			trains.push_back(train);
 		}
 		
-		printf("%d\n", ans);		
+		int ans = 0;
+		
+		while(trains.size() > 1)
+		{
+			list<int>::iterator it = find(trains.begin(), trains.end(), trains.size() );
+			
+			assert(it != trains.end());
+			
+			ans += -1 + distance(it, trains.end() );
+			
+			trains.erase(it);
+		}
+		
+		printf("Optimal train swapping takes %d swaps.\n", ans);
 	}
 
 	return 0;
