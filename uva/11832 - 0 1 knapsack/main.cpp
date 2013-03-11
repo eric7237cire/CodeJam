d3afa7083cefed4ca0873f653408bbb361a7a034
Eric Gron <eric7237cire@gmail.com>
2013-03-11 22:04:26 +0100

    h

--- uva/11832 - 0 1 knapsack/main.cpp
+++ uva/11832 - 0 1 knapsack/main.cpp
@@ -21,106 +21,124 @@ typedef unsigned int uint;
 
 char buf[1024];
 
-const int items = 30;
-const int maxSpace = 1000;
-int V[items];
-int W[items];
-int depth[items];
-char* next;
+const int MAX_N = 40;
+const int MAX_V = 80000;
 
-int maxIdx;
+int V[MAX_N];
+int N;
 
-int memo[items][1 + maxSpace];
-int used[items][1 + maxSpace];
+bool POS[MAX_N];
+bool NEG[MAX_N];
+int F;
 
-int value(int idx, int left, int& usedItems)
+char memo[MAX_N][1 + MAX_V];
+//char used[MAX_N][1 + MAX_V];
+
+int possible(int idx, int cash)
 {
-	//Considered all the coins
-	if (idx == maxIdx || left == 0)
-		return 0;
+	if (idx == N && cash == F)
+		return 'T';
 		
-	if (memo[idx][left] != -1)
+	if (idx == N)
+		return 'F';
+		
+	char& res = memo[idx][cash];
+	
+	if (res != 0)
 	{
-		usedItems |= used[idx][left];
-		return memo[idx][left];
+		return res;
 	}
 		
-		
-	//Can't use item
-	if (W[idx] > left)
-		return value(1+idx, left, usedItems);
-		
-	int usedIfSkip = usedItems;
-	int usedIfUse = usedItems;
-	int valIfSkip = value(1+idx, left, usedIfSkip);
-	int valIfUse =  V[idx] + value(1+idx, left - W[idx], usedIfUse );
+	bool canBeAdd = false;
+	bool canBeSub = false;
 	
-	if (valIfUse >= valIfSkip) {
-		//printf("Used idx memo[%d] left %d  used %d \n", idx, left,  usedIfUse);
-		usedItems |= usedIfUse | 1 << idx; 
+	if (cash - V[idx] >= 0)
+	{
+		char ifSub = possible(1+idx, cash - V[idx]);
+		if (ifSub == 'T')
+			canBeSub = true;
+	}
+	
+	if (cash + V[idx] <= MAX_V)
+	{
+		char ifAdd = possible(1+idx, cash + V[idx]);
+		if (ifAdd == 'T')
+			canBeAdd = true;
+	}
+	
+	if (canBeSub)	
+		NEG[idx] = true;
+	
+	if (canBeAdd)
+		POS[idx] = true;
+		
+	if (!canBeSub && !canBeAdd)
+	{
+		res = 'F';
 	} else {
-		usedItems |= usedIfSkip;
+		res = 'T';
 	}
 	
-	memo[idx][left] = max(valIfSkip, valIfUse);
-	used[idx][left] = usedItems;
-	//printf("memo[%d] left %d = %d  used %d \n", idx, left, memo[idx][left], usedItems);
-	return memo[idx][left];
+	//printf("memo[%d][%d or %d] = %c \n", idx, cash, cash - 40000, res);
+	
+	return res;
+	//
+	//return memo[idx][left];
 }
 
-int NumberOfSetBits(int i)
-{
-    i = i - ((i >> 1) & 0x55555555);
-    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-    return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
-}
 
 int main()
 {
-#if !ONLINE_JUDGE && _MSC_VER && !__INTEL_COMPILER
-	freopen ("input.txt","r",stdin);
-#endif
-	int t, w;
-	bool isFirst = true;
-	while(2 == scanf("%d %d", &t, &w))
+
+	while(2 == scanf("%d%d", &N, &F) && (N||F))
 	{
-		memset(memo, -1, sizeof memo);
+		memset(memo, 0, sizeof memo);
 		
-		if (isFirst) {
-			isFirst = false;
-		} else {
-			fputs("\n", stdout);
-		}
+		fill(POS, POS + N, false);
+		fill(NEG, NEG + N, false);
 		
-		scanf("%d", &maxIdx);
+		F += 40000;
 		
-		FOR(i, 0, maxIdx)
+		FOR(i, 0, N)
 		{
-			int d, v;
-			scanf("%d %d", &d, &v);
-			W[i] = 3 * w * d;
-			depth[i] = d;
+			int v;
+			scanf("%d", &v);
 			V[i] = v;
 		}
 			
-		int usedItems = 0;
-		int ans = value(0, t, usedItems);
-		
-		usedItems = used[0][t];
-		
-		int usedTreasure = NumberOfSetBits(usedItems);
+		char ans = possible(0, 40000);
 		
 		//printf("max %d Ans %d\n", maxValue, ans);
-		printf("%d\n", ans );
-		printf("%d\n", usedTreasure );
 		
-		FOR(i, 0, maxIdx)
+		
+		if (ans != 'T')
 		{
-			if (  (1 << i & usedItems) == 0)
-				continue;
-				
-			printf("%d %d\n", depth[i], V[i]);
+			printf("*\n", ans );
+			continue;			
 		}
+			
+		//printf("ans %c\n", ans );
+			
+		int cash = 40000;
+		
+		//Any transaction that is not needed cancels out, so we ignore those
+		FOR(i, 0, N)
+		{
+			//ans = possible(i, cash);
+						
+			if (POS[i] && NEG[i])
+				putc('?', stdout);
+			else if (POS[i])
+				putc('+', stdout);
+			else if (NEG[i])
+				putc('-', stdout);
+			else 
+				throw 3;
+							
+			//printf("%c", ans );
+		}
+		
+		puts("");
 	}
 
 	return 0;
