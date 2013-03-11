beac22ac6778fcc3bd83b08a978bfd0845e8cc4f
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-03-11 17:46:54 +0100

    a

--- uva/10189 - adhoc easy/main.cpp
+++ uva/10189 - adhoc easy/main.cpp
@@ -13,27 +13,63 @@ using namespace std;
 
 #define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
 #define pb push_back
- 
+int m, n;
+char grid[102][102];
+char buf[102];
+
+int dir[][2] =
+	{
+	{1, 0},
+	{1, 1},
+	{0, 1},
+	{-1, 1},
+	{-1, 0},
+	{-1, -1},
+	{0, -1},
+	{1, -1}
+	};
 int main()
 {
-	int T;
-	scanf("%d", &T);
+	//int T;
+	//scanf("%d", &T);
 	
-	while(T--)
+	while(2 == scanf("%d%d", &m, &n) && (m||n))
 	{
-		int f;
-		scanf("%d", &f);
-		
-		int ans = 0;
-		while(f--)
+		gets(buf);
+		for(int r=0; r < m; ++r)
 		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
+			fgets(buf, 102, stdin);
+			strcpy(grid[r], buf);
+			//puts(buf);
 		}
 		
-		printf("%d\n", ans);		
+		for(int r=0; r < m; ++r)
+		{
+			for(int c=0; c < n; ++c)
+			{
+				if (grid[r][c] == '*')
+				{
+					putc('*', stdout);
+					continue;
+				}
+				int tally = 0;
+				for(int d=0; d < 8; ++d)
+				{
+					int rr = r + dir[d][1];
+					int cc = c + dir[d][0];
+					
+					if (rr >= 0 && rr < m &&
+					cc >= 0 && cc < n && grid[rr][cc] == '*')
+					{
+						++tally;
+					}
+				}
+				
+				printf("%d", tally);
+			}
+			
+			puts("");
+		}
 	}
-
 	return 0;
 }
