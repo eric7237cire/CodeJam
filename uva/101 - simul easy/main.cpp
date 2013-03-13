05d711b6d3dca310a9c420d2261ddc22768ff61d
Eric Gron <eric7237cire@gmail.com>
2013-03-13 21:39:41 +0100

    sh

--- uva/101 - simul easy/main.cpp
+++ uva/101 - simul easy/main.cpp
@@ -2,155 +2,73 @@
 #include <algorithm>
 #include <cstring>
 #include <list>
-#include <cassert>
-
 using namespace std;
-
-typedef list<int> li;
-typedef li::iterator LIT;
-
-#define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
 
 const int MAX_N = 25;
 int loc[MAX_N];
-
-li piles[MAX_N];
-
 int N;
+list<int> piles[MAX_N];
 
-char cmd1[10];
-char cmd2[10];
+char cmd1[10], cmd2[10];
 
 //Return everything above pilePos to original positions
-void returnToOrig( li& l, const LIT& pilePos, int pile )
+void returnToOrig( list<int>& l, list<int>::iterator it, int pile )
 {
-	LIT it = pilePos;
-	it++;
-	while(it != l.end())
+	++it;
+	
+	while(it != l.end() )
 	{		
-		int block = *it;
+		int block = *it; //block will never be = pile ; should be impossible to put things under an existing block on its original position
 		
-		//should be impossible to put things under an existing block on its original position
-		assert(block != pile);
-		
-		//printf("moving %d to original pos\n", block);
-		
-		li& origPile = piles[  block ];
-		
-		//important to do this post++ or else the iterator won't be valid
-		origPile.splice( origPile.end(), l, it++ );
-		
+		piles[block].splice( piles[block].end(), l, it++ ); //important to do this post++ or else the iterator won't be valid after the splice
 		loc [ block ] = block;
 	}
-
 }
 
-void move(int b1, int b2, bool clearB2)
+void move(int b1, int b2, bool movePile, bool clearB2)
 {
-	li& l1 = piles[ loc[b1] ];
-	li& l2 = piles[ loc[b2] ];
+	list<int>& l1 = piles[ loc[b1] ];
+	list<int>& l2 = piles[ loc[b2] ];
 	
-	LIT b1It = find( l1.begin(), l1.end(), b1);	
+	list<int>::iterator b1It = find( l1.begin(), l1.end(), b1);	
 	
-	if (clearB2)
-	{
-		LIT b2It = find( l2.begin(), l2.end(), b2);
-		returnToOrig(l2, b2It, b2 );
-	}
+	if (clearB2)	
+		returnToOrig(l2, find( l2.begin(), l2.end(), b2), b2 );
 	
-	returnToOrig(l1, b1It, b1 );
-		
-	l2.splice( l2.end(), l1, b1It );
+	if (!movePile)
+		returnToOrig(l1, b1It, b1 );
 	
-	loc[b1] = loc[b2];
-}
-
-void pile(int b1, int b2, bool clearB2)
-{
-	li& l1 = piles[ loc[b1] ];
-	li& l2 = piles[ loc[b2] ];
-	
-	LIT b1It = find( l1.begin(), l1.end(), b1);	
-	
-	if (clearB2)
-	{
-		LIT b2It = find( l2.begin(), l2.end(), b2);	
-		returnToOrig(l2, b2It, b2);
-	}
-	
-	for( LIT it = b1It; it != l1.end(); ++it)
-	{
+	for( list<int>::iterator it = b1It; it != l1.end(); ++it)
 		loc[*it] = loc[b2];
-	}
-	
-	l2.splice( l2.end(), l1, b1It, l1.end() );	
-}
-
-void checkAll()
-{
-	FOR(i, 0, N)
-	{
-		li& l = piles[ loc[i] ];
-		assert( find( l.begin(), l.end(), i) != l.end() );
-	}
-}
-
-void printPiles()
-{
-	FOR(i, 0, N)
-	{
-		printf("%d:", i);
 		
-		for(LIT it = piles[i].begin(); it != piles[i].end(); ++it)
-			printf(" %d", *it);
-		
-		puts("");
-	}
-}	
-
-
+	l2.splice( l2.end(), l1, b1It, l1.end() );		
+}
 
 int main()
 {
-	int T;
 	int b1; int b2;
 	
 	while(1 == scanf("%d", &N))
 	{
-		scanf("%d", &N);
-		
-		//printf("N=%d\n", N);
-		
-		FOR(i, 0, N)
+		for(int i = 0; i < N; ++i)
 		{
-			//printf("i=%d\n", i);
-			piles[i].clear();
-			piles[i].push_back(i);
+			piles[i].assign(1, i);
 			loc[i] = i;
-			assert(piles[i].size() == 1);
 		}
 		
 		while(1 == scanf("%s", cmd1) && 0 != strcmp(cmd1, "quit") && 3 == scanf("%d %s %d", &b1, cmd2, &b2))
-		{
-			// printf("Read %s %d - %s %d\n", cmd1, b1, cmd2, b2);
-			if (b1 == b2 || loc[b1] == loc[b2])
-				continue;
-			
-			bool clearB2 = 0 == strcmp(cmd2, "onto");
-			
-			if ( !strcmp(cmd1, "move") )
-				::move(b1, b2, clearB2);
+			if (b1 != b2 && loc[b1] != loc[b2])
+				::move(b1, b2, 0 == strcmp(cmd1, "pile"), 0 == strcmp(cmd2, "onto"));
 				
-			if ( !strcmp(cmd1, "pile") ) 
-				::pile(b1, b2, clearB2);
+		for(int i = 0; i < N; ++i)
+		{
+			printf("%d:", i);
 			
-			checkAll();
+			for(list<int>::iterator it = piles[i].begin(); it != piles[i].end(); ++it)
+				printf(" %d", *it);
+			
+			puts("");
 		}
-		
-		assert( 0 == strcmp(cmd1, "quit") );
-		
-		printPiles();
-		
 	}
 
 	return 0;
--- uva/101 - simul easy/run.bat
+++ uva/101 - simul easy/run.bat
@@ -2,3 +2,5 @@ make all
 cat input.txt | project > out.txt
 cat input.txt | p3 > o3.txt
 diff out.txt o3.txt
+rm out.txt 
+rm o3.txt
