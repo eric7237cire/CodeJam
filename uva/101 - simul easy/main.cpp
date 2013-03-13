7b4e4c9a2ac31546a5cf160fcfe2cc8cadc96e86
Eric Gron <eric7237cire@gmail.com>
2013-03-13 20:41:35 +0100

    a

--- uva/101 - simul easy/main.cpp
+++ uva/101 - simul easy/main.cpp
@@ -45,64 +45,38 @@ void returnToOrig( li& l, const LIT& pilePos, int pile )
 
 }
 
-void moveOnto(int b1, int b2)
+void move(int b1, int b2, bool clearB2)
 {
 	li& l1 = piles[ loc[b1] ];
 	li& l2 = piles[ loc[b2] ];
 	
 	LIT b1It = find( l1.begin(), l1.end(), b1);	
-	LIT b2It = find( l2.begin(), l2.end(), b2);
 	
-	returnToOrig(l1, b1It, b1 );
-	returnToOrig(l2, b2It, b2 );
-	
-	//b2It++;	
-	l2.splice( l2.end(), l1, b1It );
-	
-	loc[b1] = loc[b2];
-}
-
-void moveOver(int b1, int b2)
-{
-	li& l1 = piles[ loc[b1] ];
-	li& l2 = piles[ loc[b2] ];
-	
-	LIT b1It = find( l1.begin(), l1.end(), b1);	
-	assert(b1It != l1.end() );
-	
-	returnToOrig(l1, b1It, b1);
-	
-	l2.splice( l2.end(), l1, b1It );
-	
-	loc[b1] = loc[b2];
-}
-
-void pileOnto(int b1, int b2)
-{
-	li& l1 = piles[ loc[b1] ];
-	li& l2 = piles[ loc[b2] ];
-	
-	LIT b1It = find( l1.begin(), l1.end(), b1);	
-	LIT b2It = find( l2.begin(), l2.end(), b2);
-	
-	returnToOrig(l2, b2It, b2);
-	
-	for( LIT it = b1It; it != l1.end(); ++it)
+	if (clearB2)
 	{
-		loc[*it] = loc[b2];
+		LIT b2It = find( l2.begin(), l2.end(), b2);
+		returnToOrig(l2, b2It, b2 );
 	}
 	
-	l2.splice( l2.end(), l1, b1It, l1.end() );	
-
-	//loc[b1] = loc[b2];
+	returnToOrig(l1, b1It, b1 );
+		
+	l2.splice( l2.end(), l1, b1It );
+	
+	loc[b1] = loc[b2];
 }
 
-void pileOver(int b1, int b2)
+void pile(int b1, int b2, bool clearB2)
 {
 	li& l1 = piles[ loc[b1] ];
 	li& l2 = piles[ loc[b2] ];
 	
 	LIT b1It = find( l1.begin(), l1.end(), b1);	
+	
+	if (clearB2)
+	{
+		LIT b2It = find( l2.begin(), l2.end(), b2);	
+		returnToOrig(l2, b2It, b2);
+	}
 	
 	for( LIT it = b1It; it != l1.end(); ++it)
 	{
@@ -126,17 +100,13 @@ void printPiles()
 	FOR(i, 0, N)
 	{
 		printf("%d:", i);
-		for(LIT it = piles[i].begin();
-		it != piles[i].end();
-		++it)
-		{
-			//if (it != piles[i].begin())
-			//	printf(" ");
+		
+		for(LIT it = piles[i].begin(); it != piles[i].end(); ++it)
 			printf(" %d", *it);
-		}
+		
 		puts("");
 	}
-}
+}	
 
 
 
@@ -165,29 +135,21 @@ int main()
 			// printf("Read %s %d - %s %d\n", cmd1, b1, cmd2, b2);
 			if (b1 == b2 || loc[b1] == loc[b2])
 				continue;
+			
+			bool clearB2 = 0 == strcmp(cmd2, "onto");
+			
+			if ( !strcmp(cmd1, "move") )
+				::move(b1, b2, clearB2);
 				
-			if ( 0 == strcmp(cmd1, "move") &&
-			0 == strcmp(cmd2, "onto") )
-				moveOnto(b1, b2);
-			if ( 0 == strcmp(cmd1, "move") &&
-			0 == strcmp(cmd2, "over") )
-				moveOver(b1, b2);
-			if ( 0 == strcmp(cmd1, "pile") &&
-			0 == strcmp(cmd2, "onto") )
-				pileOnto(b1, b2);
-			if ( 0 == strcmp(cmd1, "pile") &&
-			0 == strcmp(cmd2, "over") )
-				pileOver(b1, b2);
-				
-			// printPiles();
+			if ( !strcmp(cmd1, "pile") ) 
+				::pile(b1, b2, clearB2);
+			
 			checkAll();
 		}
 		
 		assert( 0 == strcmp(cmd1, "quit") );
 		
 		printPiles();
-		
-		//return 0;
 		
 	}
 
