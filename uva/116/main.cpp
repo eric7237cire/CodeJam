029b155936659938b245d260d1139e86ac075d2c
Eric Gron <eric7237cire@gmail.com>
2013-03-23 20:22:40 +0100

    a

---
+++ uva/116/main.cpp
@@ -0,0 +1,27 @@
+#define USING_MATH 0
+
+//STARTCOMMON
+
+//STOPCOMMON
+
+#if USING_MATH
+using namespace math
+#endif
+
+
+int main() {
+
+	int T;
+	scanf("%d", &T);
+
+	while(T--)
+	{
+		
+		if (T > 0)
+			printf("\n");
+
+		//scanf("%d", &nSeg);
+		
+	}
+	return 0;
+}
