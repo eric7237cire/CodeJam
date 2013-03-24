a24b4bdca2cfb531e1717a8612c5b876f96312c2
Eric Gron <eric7237cire@gmail.com>
2013-03-24 12:55:12 +0100

    h

--- uva/571 - dp/main.cpp
+++ uva/571 - dp/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <queue>
 #include <utility>
+#include <algorithm>
 using namespace std;
 
 #define FORE(k,a,b) for(int k=(a); k <= (b); ++k)
@@ -18,7 +19,7 @@ ii prev[1001][1001];
 
 //fill, empty, pour 
 
-
+ii choices[6];
 
 int main() 
 {
@@ -26,12 +27,11 @@ int main()
 	int Ca, Cb, N;
 	while(3 == scanf("%d%d%d", &Ca, &Cb, &N))
 	{
-		FORE(i, 0, N) FORE(j, 0, N)
+		FORE(i, 0, Ca) FORE(j, 0, Cb)
 		{
 			visited[i][j] = false;
 			prev[i][j] = make_pair(-1, -1);
 		}
-		
 				
 		queue<ii> toVisit;
 		
@@ -47,7 +47,12 @@ int main()
 				
 			visited[cur.first][cur.second] = true;
 			
-			ii choices[6];
+			if (cur.first == 0 && cur.second == N)
+			{
+				//cout << "Found " << endl;
+				break;
+			}
+			
 			//fill A
 			choices[0] = make_pair(Ca, cur.second);
 			//fill B
@@ -68,6 +73,7 @@ int main()
 			
 			FOR(i, 0, 6)
 			{
+				//printf("Cur %d/%d %d/%d -->  trying %d %d\n", cur.first, Ca, cur.second, Cb, choices[i].first, choices[i].second );
 				if (!visited[ choices[i].first ][ choices[i].second])
 				{
 					prev[ choices[i].first ][ choices[i].second] = cur;
@@ -76,6 +82,47 @@ int main()
 			}
 		
 		}
+		
+		//back track
+		ii cur(0, N);
+		vector<string> ans;
+		while( cur.first | cur.second )
+		{
+			//printf( "A=%d B=%d N=%d\n", cur.first, cur.second, N);
+			
+			ii result = cur; 
+			cur = prev[ cur.first ] [ cur.second ];
+			//printf( "prev A=%d B=%d N=%d\n", cur.first, cur.second, N);
+			
+			if (cur.first == result.first )
+			{
+				if (result.second == Cb)
+					ans.push_back("fill B");
+				else
+					ans.push_back("empty B");
+			} else if (cur.second == result.second) 
+			{
+				if (result.first == Ca)
+					ans.push_back("fill A");
+				else 
+					ans.push_back("empty A");
+					
+			} else if (cur.first != result.first && cur.second != result.second) {
+				if (result.first > cur.first)
+					ans.push_back("pour B A");
+				else
+					ans.push_back("pour A B");
+			} else {
+				ans.push_back("Ugh?");
+			}
+		}
+		reverse( ans.begin(), ans.end() );
+		for(int i = 0; i < ans.size(); ++i)
+		{
+			cout << ans[i] << endl;
+		}
+		puts("success");
+			
 		
 	}
 	return 0;
