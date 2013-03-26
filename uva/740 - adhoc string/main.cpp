d395a341d87f7463e2a57817570332d7142c7964
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-03-26 11:43:55 +0100

    a

--- uva/740 - adhoc string/main.cpp
+++ uva/740 - adhoc string/main.cpp
@@ -1,29 +1,49 @@
-#include "stdio.h"
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-#define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
- 
-int main()
-{
-	int T;
-	scanf("%d", &T);
-	
-	while(T--)
-	{
-		int f;
-		scanf("%d", &f);
-		
-		int ans = 0;
-		while(f--)
-		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
-		}
-		
-		printf("%d\n", ans);		
-	}
+int bin2int(const string &s, string::iterator start, string::iterator end) {
+  int tmp = 0;
+  int counter = 4;
+  for (; start != end; start++) {
+    if (*start == '1') {
+      tmp += (1<<counter);
+    }
+    counter--;
+  }
+  return tmp;
+}
 
-	return 0;
+int main(void) {
+  int n;
+  string input;
+  string downshift;
+  string upshift;
+  bool down;
+
+  getline(cin, downshift);
+  getline(cin, upshift);
+
+  while (getline(cin, input)) {
+    down = true;
+
+    for (int i = 0, sz = input.size(); i < sz; i += 5) {
+      n = bin2int(input, input.begin() + i, input.begin() + i + 5);
+
+      if (n == 31) {
+        down = false;
+      } else if (n == 27) {
+        down = true;
+      } else {
+        if (down) {
+          cout << downshift[n];
+        } else {
+          cout << upshift[n];
+        }
+      }
+    }
+    cout << endl;
+  }
+
+  return 0;
 }
