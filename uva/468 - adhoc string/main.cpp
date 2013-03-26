3aaec6a90902a9f453c351dc77ba05aa5f91e514
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-03-26 11:13:30 +0100

    a

--- uva/468 - adhoc string/main.cpp
+++ uva/468 - adhoc string/main.cpp
@@ -1,29 +1,86 @@
-#include "stdio.h"
-
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
 using namespace std;
 
-#define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
- 
-int main()
-{
-	int T;
-	scanf("%d", &T);
-	
-	while(T--)
-	{
-		int f;
-		scanf("%d", &f);
-		
-		int ans = 0;
-		while(f--)
-		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
-		}
-		
-		printf("%d\n", ans);		
-	}
+bool theTruthIsOutThere(const pair<char,int> &a, const pair<char,int> &b) {
+  return a.second > b.second;
+}
 
-	return 0;
+int main(void)
+{
+  int cases;
+  string input, encoded, decoded;
+
+  cin >> cases;
+
+  // \n after cases
+  getline(cin, input);
+
+  while (cases--) {
+    // blank line
+    getline(cin, input);
+
+    getline(cin, decoded);
+    getline(cin, encoded);
+
+    input = encoded;
+
+    sort(decoded.begin(), decoded.end());
+    sort(encoded.begin(), encoded.end());
+
+    vector< pair<char,int> > decoded_occurrences;
+    vector< pair<char,int> > encoded_occurrences;
+
+    int count = 1;
+    for (int i = 1, sz = decoded.size(); i < sz; i++) {
+      if (decoded[i] == decoded[i-1]) {
+        count++;
+      } else {
+        if (decoded[i-1] >= 'a' && decoded[i-1] <= 'z' || decoded[i-1] >= 'A' && decoded[i-1] <= 'Z') {
+          decoded_occurrences.push_back(make_pair(decoded[i-1], count));
+        }
+        count = 1;
+      }
+    }
+    if (decoded[decoded.size()-1] >= 'a' && decoded[decoded.size()-1] <= 'z' || decoded[decoded.size()-1] >= 'A' && decoded[decoded.size()-1] <= 'Z') {
+      decoded_occurrences.push_back(make_pair(decoded[decoded.size()-1], count));
+    }
+
+    count = 1;
+    for (int i = 0, sz = encoded.size(); i < sz; i++) {
+      if (encoded[i] == encoded[i-1]) {
+        count++;
+      } else {
+        if (encoded[i-1] >= 'a' && encoded[i-1] <= 'z' || encoded[i-1] >= 'A' && encoded[i-1] <= 'Z') {
+          encoded_occurrences.push_back(make_pair(encoded[i-1], count));
+        }
+        count = 1;
+      }
+    }
+    if (encoded[encoded.size()-1] >= 'a' && encoded[encoded.size()-1] <= 'z' || encoded[encoded.size()-1] >= 'A' && encoded[encoded.size()-1] <= 'Z') {
+      encoded_occurrences.push_back(make_pair(encoded[encoded.size()-1], count));
+    }
+
+    sort(decoded_occurrences.begin(), decoded_occurrences.end(), theTruthIsOutThere);
+    sort(encoded_occurrences.begin(), encoded_occurrences.end(), theTruthIsOutThere);
+
+    map<char,char> decoder;
+    for (int i = 0, sz = encoded_occurrences.size(); i < sz; i++) {
+      decoder[encoded_occurrences[i].first] = decoded_occurrences[i].first;
+    }
+
+    for (int i = 0, sz = input.size(); i < sz; i++) {
+      cout << decoder[input[i]];
+    }
+    cout << endl;
+
+    if (cases) {
+      cout << endl;
+    }
+  }
+  
+  return 0;
 }
--- uva/641 - adhoc string/main.cpp
+++ uva/641 - adhoc string/main.cpp
@@ -1,29 +1,37 @@
-#include "stdio.h"
+/* @JUDGE_ID: 13160EW 641 C++ */
+// 641 Do the Untwist
+// 2003/05/28
+//@BEGIN_OF_SOURCE_CODE
 
+#include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
-#define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
- 
 int main()
 {
-	int T;
-	scanf("%d", &T);
-	
-	while(T--)
-	{
-		int f;
-		scanf("%d", &f);
-		
-		int ans = 0;
-		while(f--)
-		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
-		}
-		
-		printf("%d\n", ans);		
-	}
-
-	return 0;
+        char buf[100];
+        int p[100];
+        int i,len;
+        int k,b;
+        while(1) {
+                cin >> k;
+                if( k == 0 ) break;
+                cin >> buf;
+                len = strlen(buf);
+                for(i=0;i<len;i++) {
+                        if( buf[i]=='_' ) b = 0;
+                        else if( buf[i] == '.' ) b = 27;
+                        else b= buf[i]-'a'+1;
+                        p[k*i % len] = (b+i) % 28;
+                }
+                for(i=0;i<len;i++) {
+                        if( p[i]==0) cout << "_";
+                        else if( p[i]==27) cout << ".";
+                        else cout << (char)(p[i]+'a'-1);
+                }
+                cout << endl;
+        }
+        return 0;
 }
+//@END_OF_SOURCE_CODE
