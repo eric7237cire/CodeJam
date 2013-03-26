e9f029e7003f5e7d9b20c2f8bbd11b6e6b3e1b2c
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-03-26 17:02:31 +0100

    a

--- uva/10508 - string adhoc/main.cpp
+++ uva/10508 - string adhoc/main.cpp
@@ -1,18 +1,38 @@
-//STARTCOMMON
+/* Problem: Word Morphing UVa 10508
+   Programmer: Md. Mahmud Ahsan
+   Description: Ad-hoc
+   Compiled: Visual C++ 7.0
+   Date: 13-09-06
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-//STOPCOMMON
+int main(){
+	//freopen("input.txt", "r", stdin);
+	vector <string> str;
+	string org, temp;
+	int word, letter, cnt;
 
-int main() {
+	while (cin >> word >> letter){
+		str.resize(word);
 
-	int T;
-	scanf("%d", &T);
+		cin >> org;
+		str[0] = org;
 
-	while(T--)
-	{
-		
-		if (T > 0)
-			printf("\n");
-		
+		for (int j = 1; j < word; ++j){ 
+			cin >> temp;
+			cnt = 0;
+			for (int i = 0; i < letter; ++i){
+				if (org[i] != temp[i])
+					++cnt;
+			}
+			str[cnt] = temp;
+		}
+		for (int i = 0; i < str.size(); ++i)
+			cout << str[i] << endl;
 	}
+
 	return 0;
 }
