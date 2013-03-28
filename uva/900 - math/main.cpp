71fd405fee0f3121ffe51e7c0b7e18d26c6a1c21
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-03-28 15:35:25 +0100

    a

---
+++ uva/900 - math/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    
+    int F[51];
+    
+    F[0]=F[1]=1;
+    
+    for(int i=2;i<=50;i++) F[i]=F[i-1]+F[i-2];
+    
+    int n;
+    
+    while(1){
+        scanf("%d",&n);
+        if(n==0) break;
+        
+        printf("%d\n",F[n]);
+    }
+    
+    return 0;
+}
