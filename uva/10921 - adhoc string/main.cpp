9d7721f9759a404ee55edcddf1710137a47aa271
eric <eric@vickycat.(none)>
2013-03-31 11:36:31 +0200

    a

--- uva/10921 - adhoc string/main.cpp
+++ uva/10921 - adhoc string/main.cpp
@@ -1,29 +1,41 @@
-#include "stdio.h"
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
-#define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
- 
-int main()
-{
-	int T;
-	scanf("%d", &T);
-	
-	while(T--)
-	{
-		int f;
-		scanf("%d", &f);
-		
-		int ans = 0;
-		while(f--)
-		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
-		}
-		
-		printf("%d\n", ans);		
-	}
+typedef unsigned long long ULL;
+typedef long long LL;
 
-	return 0;
+int main() {
+    string str;
+
+    while(cin >> str) {
+        for(int i=0; i<str.length(); i++) {
+            str[i] = toupper(str[i]);
+            if (str[i] == '-' || str[i] == '1' || str[i] == '0')
+                cout << str[i];
+            else {
+                if (str[i] >= 'A' && str[i] <='C')
+                    cout << "2";
+                else if (str[i] >= 'D' && str[i] <= 'F')
+                    cout << "3";
+                else if (str[i] >= 'G' && str[i] <= 'I')
+                    cout << "4";
+                else if (str[i] >= 'J' && str[i] <= 'L')
+                    cout << "5";
+                else if (str[i] >= 'M' && str[i] <= 'O')
+                    cout << "6";
+                else if (str[i] >= 'P' && str[i] <= 'S')
+                    cout << "7";
+                else if (str[i] >= 'T' && str[i] <= 'V')
+                    cout << "8";
+                else if (str[i] >= 'W' && str[i] <= 'Z')
+                    cout << "9";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
 }
--- uva/795 - adhoc string/main.cpp
+++ uva/795 - adhoc string/main.cpp
@@ -1,28 +1,92 @@
 #include "stdio.h"
-
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
 using namespace std;
 
 #define FOR(k,a,b) for(int k=(a); k <  (b); ++k)
  
+string encode(string text)
+{
+ 
+    int order[] = {8,35,17,34,7,33,
+        16,6,26,5,32,25,
+        4,15,31,24,14,3,
+    23,30,2,13,29,22,
+    1,12,21,0,20,28,
+    11,19,10,27,9,18 };
+ 
+  while (text.length() % 36 != 0)
+  {
+    text=text+'#';
+  }
+ 
+  string result;
+  for (int i=text.length()-1;i>-1;i-=36)
+  {
+    string block=text.substr(i-35,36);
+    for (int j=0;j<36;j++)
+    {
+      result += block.substr(order[j],1);
+    }
+  }
+  
+  return result;
+}   
+
+string decode(string text)
+{
+    
+    int order[] ={
+        1,3,5,10,14,19,
+        22,29,33,8,11,15,
+        18,23,26,28,31,35,
+        2,6,13,16,21,25,
+        30,32,34,0,4,7,
+    9,12,17,20,24,27};
+    
+  string result;
+ 
+  assert(text.length() % 36 == 0);
+ 
+  for (int i=0;i<text.length();i+=36)
+  {
+    string block=text.substr(i,36);
+    for (int j=0;j<36;j++)
+    {
+      result += block.substr(order[j],1);
+    }
+  }
+ 
+  while (result.substr(0,1) == "#")
+  {
+    result = result.substr(1);
+  }
+ 
+  reverse( result.begin(), result.end() );
+  /*
+  var splitText = result.split('');
+  var reverseText = splitText.reverse();
+  result = reverseText.join("");
+ */
+  return result;
+
+}
+
+
 int main()
 {
-	int T;
-	scanf("%d", &T);
 	
-	while(T--)
+	//string s = "Real Programmers use Fortran. Quiche Eaters use Pascal.";
+	//string en = encode(s);
+	
+	//cout << en << endl;
+	
+	string s;
+	while( getline(cin, s) )
 	{
-		int f;
-		scanf("%d", &f);
-		
-		int ans = 0;
-		while(f--)
-		{
-			int area, anim, env;
-			scanf("%d%d%d", &area, &anim, &env);
-			ans += area * env; //anim cancels out
-		}
-		
-		printf("%d\n", ans);		
+	     cout << decode(s) << endl;   
 	}
 
 	return 0;
