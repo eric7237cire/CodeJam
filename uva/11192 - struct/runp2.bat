fcd55a5ba2e5f25aba79eaf032068dce1dea33a2
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-04-11 17:17:28 +0200

    a

---
+++ uva/11192 - struct/runp2.bat
@@ -0,0 +1,9 @@
+python c:\codejam\codejam\lib\replaceInFile.py main.cpp common
+make clean all  > out.txt 2>&1
+cat out.txt
+g++ p2.cpp -o p2.exe
+
+gen_data.py > data.txt
+cat data.txt | project > out.txt
+cat data.txt | p2 > out2.txt
+diff out.txt out2.txt
