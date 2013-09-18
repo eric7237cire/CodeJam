008e7e63aa2eaf09b0f433eb43663bad9684d192
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-09-18 16:53:18 +0200

    a

--- cjava/src/main/java/codejam/y2013/round_1A/energy/ManageYourEnergy.java
+++ cjava/src/main/java/codejam/y2013/round_1A/energy/ManageYourEnergy.java
@@ -1,10 +1,16 @@
 package codejam.y2013.round_1A.energy;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.Pair;
+
+import com.google.common.base.Preconditions;
+import com.google.common.math.IntMath;
+import com.google.common.math.LongMath;
 
 import codejam.utils.datastructures.graph.MincostMaxflow;
 import codejam.utils.datastructures.graph.MincostMaxflow2;
@@ -13,6 +19,7 @@ import codejam.utils.main.InputFilesHandler;
 import codejam.utils.main.Runner.TestCaseInputScanner;
 import codejam.utils.math.NumericBigInt;
 import codejam.utils.multithread.Consumer.TestCaseHandler;
+import codejam.utils.utils.IntegerPair;
 
 
 public class ManageYourEnergy extends InputFilesHandler implements
@@ -56,10 +63,79 @@ TestCaseHandler<InputData>, TestCaseInputScanner<InputData>
     @Override
     public String handleCase(InputData in) 
     {
-        return handleBigInts(in);
+        //return handleMincostMaxflowBigInts(in);
+        return handleUsingSolutionN2(in);
     }
     
-    public String handleBigInts(InputData in) 
+    public String handleUsingSolutionN2(InputData in)
+    {
+        int[] energyAvail = new int[in.nActivity];
+        int[] mustLeaveWith = new int[in.nActivity];
+        
+        //First is value, second is index
+        IntegerPair[] sortedAct = new IntegerPair[in.nActivity];
+        
+        boolean[] used = new boolean[in.nActivity];
+        
+        log.debug("Initial/max energy {} recup {}", in.maxEnergy, in.energyRecup);
+        
+        for(int act = 0; act < in.nActivity; ++act)
+        {
+            log.debug("Activity {} value {}", act+1, in.actValue[act]);
+            energyAvail[act] = in.maxEnergy;
+            mustLeaveWith[act] = 0;
+            sortedAct[act] = new IntegerPair(in.actValue[act], act);
+        }
+        
+        Arrays.sort(sortedAct);
+        
+        long ans = 0;
+        for(int sortAct = sortedAct.length-1; sortAct >= 0; --sortAct)
+        {
+            final int index = sortedAct[sortAct].second();
+            final int energy = energyAvail[index] - mustLeaveWith[index];
+            Preconditions.checkState(energy >= 0);
+            
+            ans += LongMath.checkedMultiply(energy, in.actValue[index]);
+            log.debug("On activity {} spent {} for a gain of {}.  ans {}",
+                    index+1, energy, energy * in.actValue[index], ans);
+            
+            used[index] = true;
+            
+            for(int idx = index-1;  idx >= 0; --idx)
+            {
+                if (used[idx])
+                    break;
+                int dist = index - idx;
+                long energyCantUse = (long)energyAvail[index] - LongMath.checkedMultiply(in.energyRecup, dist);
+                //Preconditions.checkState(energyCantUse <= canSpend[idx]);
+                //canSpend[idx] -= energyCantUse;
+                Preconditions.checkState((long)Integer.MAX_VALUE >= Math.max(energyCantUse, mustLeaveWith[idx]));
+                mustLeaveWith[idx] = (int) Math.max(energyCantUse, mustLeaveWith[idx]);
+                log.debug("before {}.  must reserve {} / {} on activity {}", index+1, 
+                        mustLeaveWith[idx], energyAvail[idx], idx+1);
+            }
+            
+            for(int idx = index + 1; idx < in.nActivity; ++idx)
+            {
+                if (used[idx])
+                    break;
+                
+                int maxEnergy = (int) Math.min((long)mustLeaveWith[index]
+                        +LongMath.checkedMultiply(in.energyRecup,(idx-index)),
+                        (long)in.maxEnergy);
+                Preconditions.checkState(maxEnergy <= energyAvail[idx]);
+                energyAvail[idx] = maxEnergy;
+                log.debug("after {}.  Can spend {} on activity {}", index+1, energyAvail[idx],idx+1);
+            }
+        }
+        
+        return String.format("Case #%d: %d", 
+                in.testCase, ans
+                );
+    }
+    
+    public String handleMincostMaxflowBigInts(InputData in) 
     {
         MincostMaxflow<BigInteger, BigInteger> flow = new MincostMaxflow<>(new NumericBigInt(0L), new NumericBigInt(0L));
         
@@ -110,7 +186,7 @@ TestCaseHandler<InputData>, TestCaseInputScanner<InputData>
        
        
     }
-    public String handle1(InputData in) 
+    public String handleMincostMaxflowInt(InputData in) 
     {
         List<Edge>[] graph = MincostMaxflow2.createGraph(2+2*in.nActivity);
         int sink = 1+2*in.nActivity;
