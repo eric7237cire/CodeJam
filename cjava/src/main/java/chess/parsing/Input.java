bb1c96610a149e2e983507eab0bc60468ddab3c2
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-10-25 17:03:06 +0200

    a

---
+++ cjava/src/main/java/chess/parsing/Input.java
@@ -0,0 +1,79 @@
+package chess.parsing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Input {
+    
+    static Logger log = LoggerFactory.getLogger(Input.class);
+    
+    CharSequence curBlock;
+    private int curMarker;
+    
+    StringBuffer sb = new StringBuffer();
+    
+    private InputStream is;
+    BufferedReader br;
+    
+    private final int minBlockLength = 255;
+    
+    boolean doneReading = false;
+    
+    public Input(InputStream is) {
+        super();
+        this.is = is;
+        
+        br = new BufferedReader(new InputStreamReader(is));
+    }
+
+
+
+    public CharSequence getCurrentBlock()
+    {
+        if (doneReading) {
+            return sb.subSequence(curMarker, sb.length());
+        }
+        while( ( (sb.length() - curMarker) <  minBlockLength) )
+        {
+            try {
+                String line = br.readLine();
+                
+                if (line == null) 
+                {
+                    doneReading = true;
+                    break;
+                }
+                
+                sb.append(line);
+            } catch (IOException ex) {
+                log.error("ex",ex);
+                doneReading = true;
+            }
+        }
+        
+        return sb.subSequence(curMarker, sb.length());
+    }
+
+
+
+    /**
+     * @return the curMarker
+     */
+    public int getCurMarker() {
+        return curMarker;
+    }
+
+
+
+    /**
+     * @param curMarker the curMarker to set
+     */
+    public void setCurMarker(int curMarker) {
+        this.curMarker = curMarker;
+    }
+}
---
+++ cjava/src/test/java/chess/TestPgn.java
@@ -0,0 +1,53 @@
+package chess;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import chess.parsing.Input;
+import chess.parsing.PgnParser;
+import chess.parsing.TagName;
+
+public class TestPgn {
+    
+    Logger log = LoggerFactory.getLogger(TestPgn.class);
+    
+    @Test
+    public void test() throws IOException
+    {
+        InputStream is = TestPgn.class.getResourceAsStream(
+                "test1.pgn");
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        
+        String line;
+        while( (line = reader.readLine()) != null )
+        {
+            log.debug(line);
+        }
+        
+        Pair<TagName, String> tag1 = PgnParser.parseTag("[TimeControl \"20/300:0\"]");
+        assertEquals(TagName.TimeControl, tag1.getKey());
+        assertEquals("20/300:0", tag1.getValue()); 
+        
+    }
+    
+    @Test
+    public void test2() 
+    {
+        
+        Input input = new Input(TestPgn.class.getResourceAsStream(
+                "test1.pgn"));
+        PgnParser p = new PgnParser(input);
+        
+        p.parse();
+    }
+}
