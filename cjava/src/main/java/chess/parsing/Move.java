bdbba21a34f1fb5fcaa8c00a35cc57f72960fd8b
unknown <Groning-E@B016653.fr.ad.bull.net>
2013-10-28 16:11:35 +0100

    a

--- cjava/src/main/java/chess/parsing/Move.java
+++ cjava/src/main/java/chess/parsing/Move.java
@@ -20,4 +20,40 @@ public class Move {
     public String toString() {
         return moveNumber + ". " + whiteMove.sanTxt + " " + (blackMove != null ? blackMove.sanTxt : '*');
     }
+    /**
+     * @return the moveNumber
+     */
+    public int getMoveNumber() {
+        return moveNumber;
+    }
+    /**
+     * @param moveNumber the moveNumber to set
+     */
+    public void setMoveNumber(int moveNumber) {
+        this.moveNumber = moveNumber;
+    }
+    /**
+     * @return the whiteMove
+     */
+    public Ply getWhiteMove() {
+        return whiteMove;
+    }
+    /**
+     * @param whiteMove the whiteMove to set
+     */
+    public void setWhiteMove(Ply whiteMove) {
+        this.whiteMove = whiteMove;
+    }
+    /**
+     * @return the blackMove
+     */
+    public Ply getBlackMove() {
+        return blackMove;
+    }
+    /**
+     * @param blackMove the blackMove to set
+     */
+    public void setBlackMove(Ply blackMove) {
+        this.blackMove = blackMove;
+    }
 }
--- cjava/src/test/java/chess/TestPgn.java
+++ cjava/src/test/java/chess/TestPgn.java
@@ -2,10 +2,7 @@ package chess;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
@@ -13,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import chess.parsing.Input;
+import chess.parsing.Move;
 import chess.parsing.PgnParser;
 import chess.parsing.TagName;
 
@@ -38,6 +36,60 @@ public class TestPgn {
         Pair<TagName, String> tag1 = PgnParser.parseTag("[TimeControl \"20/300:0\"]");
         assertEquals(TagName.TimeControl, tag1.getKey());
         assertEquals("20/300:0", tag1.getValue()); 
+        
+    }
+    
+    @Test
+    public void parseMove()
+    {
+        Input input = new Input("45. d5 {test comment} e4");
+        PgnParser p = new PgnParser(input);
+        
+        Move m = p.parseMove();
+        
+        assertEquals(45, m.getMoveNumber());
+        assertEquals("d5", m.getWhiteMove().getSanTxt());
+        assertEquals("test comment", m.getWhiteMove().getComment());
+        
+        assertEquals("e4", m.getBlackMove().getSanTxt());
+    }
+    
+    @Test
+    public void parseVariation()
+    {
+        Input input = new Input("45. d5 {test comment} (45. N3xe4 $2) 45... e4");
+        PgnParser p = new PgnParser(input);
+        
+        Move m = p.parseMove();
+        
+        assertEquals(45, m.getMoveNumber());
+        assertEquals("d5", m.getWhiteMove().getSanTxt());
+        assertEquals("test comment", m.getWhiteMove().getComment());
+        assertEquals(1, m.getWhiteMove().getVariations().size());
+        
+        Move varMove1 = m.getWhiteMove().getVariations().get(0).get(0);
+        assertEquals("N3xe4", varMove1.getWhiteMove().getSanTxt());
+        assertEquals((Integer)2, varMove1.getWhiteMove().getNagValue());
+        
+        assertEquals("e4", m.getBlackMove().getSanTxt());
+    }
+    
+    @Test
+    public void parseVariations()
+    {
+        Input input = new Input("45. d5 {test comment} (45. N3xe4 $2) 45... e4 (45... Nexh3) (45... Nbxh3) 46.  f3 *");
+        PgnParser p = new PgnParser(input);
+        
+        Move m = p.parseMove();
+        
+
+        assertEquals( 2, m.getBlackMove().getVariations().size() );
+        
+        Move var1Move1 = m.getBlackMove().getVariations().get(0).get(0);
+        Move var2Move1 = m.getBlackMove().getVariations().get(1).get(0);
+        
+        assertEquals("Nexh3", var1Move1.getBlackMove().getSanTxt());
+        assertEquals("Nbxh3", var2Move1.getBlackMove().getSanTxt());
         
     }
     
