6a8ce98a8f5b744ffa6c21d475a3e30f0cdc51fa
eg-novelt <eg@novel-t.ch>
2017-12-30 13:34:35 +0100

    2017 Qual Problem C - Small 1

--- 2017/qual/c.py
+++ 2017/qual/c.py
@@ -1,6 +1,8 @@
 
 from typing import Tuple
 import numpy as np
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
 
 def solve_brute_force(n:int, k:int) -> Tuple[int,int]:
 
@@ -8,14 +10,20 @@ def solve_brute_force(n:int, k:int) -> Tuple[int,int]:
     stalls[0] = True
     stalls[-1] = True
 
-    max_min = -1
-    min_max = 1e100
-    chosen_pos = -1
+    if n == k:
+        return 0, 0
 
     print(f"Starting {n} {k}")
     for i in range(0, k):
 
+        max_min = -1
+        max_max = 0
+        chosen_pos = -1
+
         for pos in range(1, n+2):
+
+            if stalls[pos]:
+                continue
 
             # Find left distance
             for lpos in range(pos-1, -1, -1):
@@ -28,24 +36,25 @@ def solve_brute_force(n:int, k:int) -> Tuple[int,int]:
                     right_position = rpos
                     break
 
-            my_min = min(left_position, right_position)
-            my_max = max(left_position, right_position)
+            my_min = min(pos-left_position-1, right_position-pos-1)
+            my_max = max(pos-left_position-1, right_position-pos-1)
 
             if my_min > max_min:
                 chosen_pos = pos
                 max_min = my_min
+                max_max = my_max
 
             elif my_min >= max_min:
 
-                if my_max < min_max:
+                if my_max > max_max:
                     chosen_pos = pos
-                    min_max = my_max
+                    max_max = my_max
 
         stalls[chosen_pos] = True
 
         #print(stalls)
 
-    return max_min, min_max
+    return max_max, max_min
 
 def main():
 
@@ -58,18 +67,24 @@ def main():
     input_file_name = f"C-{file_base}-practice{ext}.in"
     output_file_name = f"C-{file_base}-practice{ext}.out"
 
-    with open(output_file_name, "w") as output_file, open(input_file_name) as input_file:
+    with open(output_file_name, "w") as output_file, \
+            ProcessPoolExecutor(max_workers = 7) as executor, \
+            open(input_file_name) as input_file:
 
         n_cases = int(input_file.readline())
 
-        for i in range(n_cases):
+        results = []
+
+        for i in range( n_cases):
 
             n, k = input_file.readline().split(" ")
 
+            results.append(executor.submit(solve_brute_force, int(n), int(k)))
 
+        for i in range( 0, len(results) ):
             output_file.write(f"Case #{i+1}: ")
 
-            mx, mn = solve_brute_force( int(n), int(k) )
+            mx, mn = results[i].result()
             output_file.write( f"{mx} {mn}" )
 
             output_file.write("\n")
