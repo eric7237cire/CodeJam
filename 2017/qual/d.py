5c145f67c047e2e2403a5b9f042d61259efa334e
eg-novelt <eg@novel-t.ch>
2017-12-31 16:46:34 +0100

    Some progress

--- 2017/qual/d.py
+++ 2017/qual/d.py
@@ -8,7 +8,8 @@
 import sys
 import numpy as np
 
-# Adapted from https://stackoverflow.com/questions/42318343/avoid-duplicates-in-n-queen-iterative-solutions-no-recursion-allowed
+# Adapted from
+# https://stackoverflow.com/questions/42318343/avoid-duplicates-in-n-queen-iterative-solutions-no-recursion-allowed
 class Board:
 
     INVALID = "#"
@@ -19,6 +20,10 @@ class Board:
         self.rooks = []
         self.bishops = []
         self.blanks = []
+
+        self.existing_pieces = []
+
+        self.board = np.empty(shape = (self.N, self.N))
 
     def convert_to_tilted_board_coords(self, row, col):
         return  row+col  , col - row + self.N
@@ -34,34 +39,37 @@ class Board:
         # 3, 1 => 1, 2
 
     def unpivot_board(self):
+        self.tilted_board = self.board
         self.board = np.empty(shape=(self.N, self.N))
 
         for row in range(0, self.tilted_board.shape[0]):
             for col in range(0, self.tilted_board.shape[1]):
-                if self.tilted_board[row, col] == Board.INVALID:
+                if self.tilted_board[row, col] == False:
                     continue
                 coords = self.convert_to_board_coords(row, col)
-                print(f"{row},{col} {self.tilted_board[row][col]}==> {coords}")
+                #print(f"{row},{col} {self.tilted_board[row][col]}==> {coords}")
                 self.board[coords[0], coords[1]] = self.tilted_board[row,col]
 
 
 
-    def pivot_board(self):
+    def create_pivot_board(self):
 
-        self.tilted_board = np.full( shape=(2*self.N, 2*self.N),
-                                     fill_value = Board.INVALID,
-                                     dtype=np.chararray)
+        self.pivot_board = np.full( shape=(2*self.N, 2*self.N),
+                                     fill_value = False,
+                                     dtype=np.bool)
 
         for row in range(0, self.N):
             for col in range(0, self.N):
                 # 45 rotation, x+y, y-x
                 # and a translation up N to avoid nulls
                 coords = self.convert_to_tilted_board_coords(row, col)
-                self.tilted_board[coords[0], coords[1]] = col + row * self.N
+                self.pivot_board[coords[0], coords[1]] = True
 
                 check_coords = self.convert_to_board_coords(*coords)
                 if (row,col) != check_coords:
                     raise Exception("problem")
+
+        self.board = self.pivot_board
 
     def is_queen_safe(self, row, col):
         for r, c in enumerate(self.queens):
@@ -78,23 +86,30 @@ class Board:
 
 
     def is_rook_safe(self, row, col):
+        if self.board[row, col] == Board.INVALID:
+            return False
+
         for r, c in enumerate(self.rooks):
             if r == row or c == col :
                 return False
+
+        for r, c in self.existing_pieces:
+            if r == row or c == col :
+                return False
+
         return True
 
     def print_the_board(self):
         print ("solution:")
-        for row in range(self.N):
-            line = ['.'] * self.N
+        self.board = np.full(shape = (self.N, self.N),
+                             fill_value = '.',
+                             dtype = np.chararray)
 
-            if self.bishops[row] == self.rooks[row]:
-                line[self.bishops[row]] = 'o'
-            else:
-                line[self.bishops[row]] = '+'
-                line[self.rooks[row]] = 'x'
+        for row, col in self.bishops:
+            self.board[row, col]='b'
 
-            print(''.join(line))
+
+        print(self.board)
 
     def solution(self, is_rooks):
 
@@ -103,38 +118,50 @@ class Board:
             safe_fn = self.is_rook_safe
         else:
             piece_array = self.bishops
-            safe_fn = self.is_bishop_safe
+            safe_fn = self.is_rook_safe
+            self.create_pivot_board()
+
+        n_rows = self.board.shape[0]
+        n_cols = self.board.shape[1]
+
+
         piece_array.clear()
-        col = row = 0
-        while True:
-            while col < self.N and not safe_fn(row, col):
-                col += 1
-            if col < self.N:
-                piece_array.append(col)
-                if row + 1 >= self.N:
-                    #self.print_the_board()
-                    return
-                    piece_array.pop()
-                    col = self.N
-                else:
-                    row += 1
-                    col = 0
-            if col >= self.N:
-                # not possible to place a queen in this row anymore
-                if row == 0:
-                    return # all combinations were tried
-                col = piece_array.pop() + 1
-                row -= 1
+
+        for i in range(0, n_rows):
+            # Find row with smallest number of empty columns (value 0)
+            row_sums = np.sum(self.board, axis = 1)
+
+            # Need to make rows with no spots unattractive
+            row_sums[row_sums == 0] = 3 * n_rows
+            # Find first free column
+            min_row = np.argmin(row_sums)
+
+            min_col = np.argmax(self.board[min_row])
+
+            if self.board[min_row, min_col] == False:
+                break
+
+            piece_array.append(self.convert_to_board_coords(min_row, min_col))
+
+            self.board[min_row] = False
+            self.board[:,min_col] = False
+
+
 
 q = Board(5)
 
-q.pivot_board()
+#q.solution(is_rooks = True)
+q.solution(is_rooks = False)
 
-print(q.tilted_board)
+q.print_the_board()
 
-q.unpivot_board()
+#q.create_pivot_board()
 
-print(q.board)
+#print(q.board)
+
+#q.unpivot_board()
+
+#print(q.board)
 
 sys.exit(0)
 
